34dc85eafec5730630c2be6f34c568ec0cc2b600
notarybob <serjoksalot@yahoo.com>
2025-01-01 07:29:40 +0000

    Revert tests - updating docs

--- packages/api/src/crm/user/services/attio/mappers.ts
+++ packages/api/src/crm/user/services/attio/mappers.ts
@@ -54,9 +54,9 @@ export class AttioUserMapper implements IUserMapper {
       remote_id: string;
     }[],
   ): Promise<UnifiedCrmUserOutput> {
-    const field_mappings: { [key: string]: any } = {};
+    let field_mappings: { [key: string]: any } = {};
     if (customFieldMappings) {
-      for (const mapping of customFieldMappings) {
+      for (let mapping of customFieldMappings) {
         field_mappings[mapping.slug] = user[mapping.remote_id];
       }
     }
