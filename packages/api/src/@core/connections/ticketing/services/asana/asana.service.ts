7c190a8ee0982fdf29788788c7a6c64038e06314
notarybob <serjoksalot@yahoo.com>
2025-01-01 22:43:27 +0000

    Improve tests - handling edge cases

--- packages/api/src/@core/connections/ticketing/services/asana/asana.service.ts
+++ packages/api/src/@core/connections/ticketing/services/asana/asana.service.ts
@@ -55,10 +55,10 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -90,8 +90,8 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'asana',
@@ -100,23 +100,23 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
       });
 
       //reconstruct the redirect URI that was passed in the githubend it must be the same
-      var REDIRECT_URI = `${
+      const REDIRECT_URI = `${
         this.env.getPanoraBaseUrl()
       }/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://app.asana.com/-/oauth_token`,
         formData.toString(),
         {
@@ -125,13 +125,13 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: AsanaOAuthResponse = res.data;
+      const data: AsanaOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : asana ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
       //get the right BASE URL API
 
       if (isNotUnique) {
@@ -183,18 +183,18 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://app.asana.com/-/oauth_token`,
         formData.toString(),
         {
@@ -203,7 +203,7 @@ export class AsanaConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: AsanaOAuthResponse = res.data;
+      const data: AsanaOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
