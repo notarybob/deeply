50c6891590e4acffd3d56e3effb8ecf89145eae0
notarybob <serjoksalot@yahoo.com>
2025-01-01 17:16:38 +0000

    Improve UI - refactoring code

--- packages/api/src/accounting/payment/payment.controller.ts
+++ packages/api/src/accounting/payment/payment.controller.ts
@@ -66,11 +66,11 @@ export class PaymentController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.paymentService.getPayments(
         connectionId,
         projectId,
@@ -119,7 +119,7 @@ export class PaymentController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -162,7 +162,7 @@ export class PaymentController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
