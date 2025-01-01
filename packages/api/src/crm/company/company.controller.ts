b44b2281ab9279a8cdeb410b0a31ba4afe129656
notarybob <serjoksalot@yahoo.com>
2025-01-01 18:28:54 +0000

    Improve dependencies - handling edge cases

--- packages/api/src/crm/company/company.controller.ts
+++ packages/api/src/crm/company/company.controller.ts
@@ -65,11 +65,11 @@ export class CompanyController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.companyService.getCompanies(
         connectionId,
         projectId,
@@ -117,7 +117,7 @@ export class CompanyController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -159,7 +159,7 @@ export class CompanyController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
