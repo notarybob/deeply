d50a246bfadcfc06a09b9b1d0cb1b006627d81a8
notarybob <serjoksalot@yahoo.com>
2025-01-04 12:41:13 +0000

    Remove database - removing deprecated function

--- packages/api/src/@core/utils/decorators/utils.ts
+++ packages/api/src/@core/utils/decorators/utils.ts
@@ -21,7 +21,7 @@ interface ProviderMetadata {
 }
 
 export async function generatePanoraParamsSpec(spec: any) {
-  const verticals = {
+  let verticals = {
     crm: [CRM_PROVIDERS, CrmObject],
     accounting: [ACCOUNTING_PROVIDERS, AccountingObject],
     ticketing: [TICKETING_PROVIDERS, TicketingObject],
@@ -33,13 +33,13 @@ export async function generatePanoraParamsSpec(spec: any) {
     ecommerce: [ECOMMERCE_PROVIDERS, EcommerceObject],
   };
 
-  for (const [vertical, [providers, COMMON_OBJECTS]] of Object.entries(
+  for (let [vertical, [providers, COMMON_OBJECTS]] of Object.entries(
     verticals,
   )) {
-    for (const objectKey of Object.values(COMMON_OBJECTS)) {
-      for (const provider of providers as string[]) {
+    for (let objectKey of Object.values(COMMON_OBJECTS)) {
+      for (let provider of providers as string[]) {
         try {
-          const metadataPath = path.join(
+          let metadataPath = path.join(
             process.cwd(),
             'src',
             vertical.toLowerCase(),
@@ -49,13 +49,13 @@ export async function generatePanoraParamsSpec(spec: any) {
             'metadata.json',
           );
 
-          const metadataRaw = fs.readFileSync(metadataPath, 'utf8');
-          const metadata: ProviderMetadata = JSON.parse(metadataRaw);
+          let metadataRaw = fs.readFileSync(metadataPath, 'utf8');
+          let metadata: ProviderMetadata = JSON.parse(metadataRaw);
 
           if (metadata) {
             metadata.actions.forEach((action, index) => {
-              const path = `/${vertical.toLowerCase()}/${objectKey}s`;
-              const op =
+              let path = `/${vertical.toLowerCase()}/${objectKey}s`;
+              let op =
                 action === 'list' ? 'get' : action === 'create' ? 'post' : '';
 
               if (spec.paths[path] && spec.paths[path][op]) {
@@ -69,7 +69,7 @@ export async function generatePanoraParamsSpec(spec: any) {
                   spec.paths[path][op]['x-panora-remote-platforms'][provider] =
                     []; // Initialize as an array
                 }
-                for (const field of metadata.supportedFields[index]) {
+                for (let field of metadata.supportedFields[index]) {
                   spec.paths[path][op]['x-panora-remote-platforms'][
                     provider
                   ].push(field);
