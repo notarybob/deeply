c064cae22c700439e9f1f4a1fc0c57524db89b86
notarybob <serjoksalot@yahoo.com>
2025-01-05 12:40:40 +0000

    Refactor tests - fixing typo

--- packages/api/src/marketingautomation/template/template.controller.ts
+++ packages/api/src/marketingautomation/template/template.controller.ts
@@ -64,11 +64,11 @@ export class TemplateController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.templateService.getTemplates(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class TemplateController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -161,7 +161,7 @@ export class TemplateController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
