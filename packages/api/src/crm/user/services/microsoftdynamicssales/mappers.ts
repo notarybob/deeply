0fdcc2545c2f902bfd3bf208649ef82b821c7bde
notarybob <serjoksalot@yahoo.com>
2025-01-05 21:37:50 +0000

    Document build - handling edge cases

--- packages/api/src/crm/user/services/microsoftdynamicssales/mappers.ts
+++ packages/api/src/crm/user/services/microsoftdynamicssales/mappers.ts
@@ -54,9 +54,9 @@ export class MicrosoftdynamicssalesUserMapper implements IUserMapper {
             remote_id: string;
         }[],
     ): Promise<UnifiedCrmUserOutput> {
-        const field_mappings: { [key: string]: any } = {};
+        let field_mappings: { [key: string]: any } = {};
         if (customFieldMappings) {
-            for (const mapping of customFieldMappings) {
+            for (let mapping of customFieldMappings) {
                 field_mappings[mapping.slug] = user[mapping.remote_id];
             }
         }
