bb50a24122f20030ecfc61ad83ccb0582da9384c
notarybob <serjoksalot@yahoo.com>
2025-01-09 23:03:10 +0000

    Update build - improving performance

--- packages/api/src/accounting/address/address.controller.ts
+++ packages/api/src/accounting/address/address.controller.ts
@@ -64,11 +64,11 @@ export class AddressController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.addressService.getAddresss(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class AddressController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
