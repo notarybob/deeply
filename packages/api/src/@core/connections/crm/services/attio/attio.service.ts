20645efbeb7b7c95e2edb13d8e81adbdcba3b4b0
notarybob <serjoksalot@yahoo.com>
2025-01-09 05:17:04 +0000

    Change styles - fixing a bug

--- packages/api/src/@core/connections/crm/services/attio/attio.service.ts
+++ packages/api/src/@core/connections/crm/services/attio/attio.service.ts
@@ -51,10 +51,10 @@ export class AttioConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -89,11 +89,11 @@ export class AttioConnectionService extends AbstractBaseConnectionService {
   }
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, code } = opts;
+      var { linkedUserId, projectId, code } = opts;
       this.logger.log(
         'linkeduserid is ' + linkedUserId + ' inside callback attio',
       );
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'attio',
@@ -102,13 +102,13 @@ export class AttioConnectionService extends AbstractBaseConnectionService {
       });
 
       //reconstruct the redirect URI that was passed in the frontend it must be the same
-      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         grant_type: 'authorization_code',
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
@@ -116,7 +116,7 @@ export class AttioConnectionService extends AbstractBaseConnectionService {
         code: code,
       });
 
-      const res = await axios.post(
+      var res = await axios.post(
         'https://app.attio.com/oauth/token',
         formData.toString(),
         {
@@ -126,11 +126,11 @@ export class AttioConnectionService extends AbstractBaseConnectionService {
         },
       );
 
-      const data: AttioOAuthResponse = res.data;
+      var data: AttioOAuthResponse = res.data;
 
       // Saving the token of customer inside db
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
 
       if (isNotUnique) {
         // Update existing connection
