b484bf5f4732ee6efb95efc38e781236a76d2b62
notarybob <serjoksalot@yahoo.com>
2025-01-11 21:07:24 +0000

    Document database - improving security

--- packages/api/src/marketingautomation/contact/contact.controller.ts
+++ packages/api/src/marketingautomation/contact/contact.controller.ts
@@ -64,11 +64,11 @@ export class ContactController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.contactService.getContacts(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class ContactController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -162,7 +162,7 @@ export class ContactController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
