2af7eadaf5e39476a67a453b8abb8f7490be4d2f
notarybob <serjoksalot@yahoo.com>
2025-01-12 21:23:39 +0000

    Fix API - removing deprecated function

--- packages/api/src/ticketing/team/services/front/index.ts
+++ packages/api/src/ticketing/team/services/front/index.ts
@@ -27,9 +27,9 @@ export class FrontService implements ITeamService {
 
   async sync(data: SyncParam): Promise<ApiResponse<FrontTeamOutput[]>> {
     try {
-      const { linkedUserId } = data;
+      let { linkedUserId } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'front',
@@ -37,7 +37,7 @@ export class FrontService implements ITeamService {
         },
       });
 
-      const resp = await axios.get(`${connection.account_url}/teams`, {
+      let resp = await axios.get(`${connection.account_url}/teams`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
