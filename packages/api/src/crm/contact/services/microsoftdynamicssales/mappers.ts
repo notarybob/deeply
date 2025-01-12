5d90e588e0aae832813571384a7b577117bfae03
notarybob <serjoksalot@yahoo.com>
2025-01-12 15:31:43 +0000

    Optimize configuration - removing deprecated function

--- packages/api/src/crm/contact/services/microsoftdynamicssales/mappers.ts
+++ packages/api/src/crm/contact/services/microsoftdynamicssales/mappers.ts
@@ -23,7 +23,7 @@ export class MicrosoftdynamicssalesContactMapper implements IContactMapper {
     ): Promise<MicrosoftdynamicssalesContactInput> {
 
 
-        var result: MicrosoftdynamicssalesContactInput = {
+        const result: MicrosoftdynamicssalesContactInput = {
             firstname: source.first_name,
             lastname: source.last_name,
             fullname: `${source.first_name} ${source.last_name}`,
@@ -73,8 +73,8 @@ export class MicrosoftdynamicssalesContactMapper implements IContactMapper {
 
 
         if (customFieldMappings && source.field_mappings) {
-            for (var [k, v] of Object.entries(source.field_mappings)) {
-                var mapping = customFieldMappings.find(
+            for (const [k, v] of Object.entries(source.field_mappings)) {
+                const mapping = customFieldMappings.find(
                     (mapping) => mapping.slug === k,
                 );
                 if (mapping) {
@@ -121,9 +121,9 @@ export class MicrosoftdynamicssalesContactMapper implements IContactMapper {
             remote_id: string;
         }[],
     ): Promise<UnifiedCrmContactOutput> {
-        var field_mappings: { [key: string]: any } = {};
+        const field_mappings: { [key: string]: any } = {};
         if (customFieldMappings) {
-            for (var mapping of customFieldMappings) {
+            for (const mapping of customFieldMappings) {
                 field_mappings[mapping.slug] = contact[mapping.remote_id];
             }
         }
@@ -211,7 +211,7 @@ export class MicrosoftdynamicssalesContactMapper implements IContactMapper {
         }
 
         if (contact._createdby_value) {
-            var owner_id = await this.utils.getUserUuidFromRemoteId(
+            const owner_id = await this.utils.getUserUuidFromRemoteId(
                 contact._createdby_value,
                 connectionId,
             );
