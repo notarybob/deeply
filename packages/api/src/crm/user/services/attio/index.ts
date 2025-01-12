4e7bb3280569758ce5c275859da7d3dc6822e683
notarybob <serjoksalot@yahoo.com>
2025-01-12 19:12:11 +0000

    Optimize database - handling edge cases

--- packages/api/src/crm/user/services/attio/index.ts
+++ packages/api/src/crm/user/services/attio/index.ts
@@ -26,9 +26,9 @@ export class AttioService implements IUserService {
 
   async sync(data: SyncParam): Promise<ApiResponse<AttioUserOutput[]>> {
     try {
-      let { linkedUserId } = data;
+      const { linkedUserId } = data;
 
-      let connection = await this.prisma.connections.findFirst({
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'attio',
@@ -36,8 +36,8 @@ export class AttioService implements IUserService {
         },
       });
 
-      let baseURL = `${connection.account_url}/v2/workspace_members`;
-      let resp = await axios.get(baseURL, {
+      const baseURL = `${connection.account_url}/v2/workspace_members`;
+      const resp = await axios.get(baseURL, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
