71cade27c9ccf562773caaabd0714f6ed82c473e
notarybob <serjoksalot@yahoo.com>
2025-01-12 07:50:42 +0000

    Improve UI - adding new feature

--- packages/api/src/@core/connections/filestorage/services/google_drive/google_drive.service.ts
+++ packages/api/src/@core/connections/filestorage/services/google_drive/google_drive.service.ts
@@ -60,10 +60,10 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -95,8 +95,8 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, code } = opts;
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var { linkedUserId, projectId, code } = opts;
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'googledrive',
@@ -104,21 +104,21 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
         },
       });
 
-      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
 
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         redirect_uri: REDIRECT_URI,
         code: code,
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'authorization_code',
       });
-      const res = await axios.post(
+      var res = await axios.post(
         `https://oauth2.googleapis.com/token`,
         formData.toString(),
         {
@@ -127,13 +127,13 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
           },
         },
       );
-      const data: GoogleDriveOAuthResponse = res.data;
+      var data: GoogleDriveOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : google drive filestorage ' + JSON.stringify(data),
       );
 
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
           where: {
@@ -189,21 +189,21 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
   }
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      const { connectionId, refreshToken, projectId } = opts;
+      var { connectionId, refreshToken, projectId } = opts;
 
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
       });
 
-      const res = await axios.post(
+      var res = await axios.post(
         `https://oauth2.googleapis.com/token`,
         formData.toString(),
         {
@@ -215,10 +215,10 @@ export class GoogleDriveConnectionService extends AbstractBaseConnectionService 
           },
         },
       );
-      const data: GoogleDriveOAuthResponse = res.data;
+      var data: GoogleDriveOAuthResponse = res.data;
 
       // Prepare the update data
-      const updateData: any = {
+      var updateData: any = {
         access_token: this.cryptoService.encrypt(data.access_token),
         expiration_timestamp: new Date(
           new Date().getTime() + Number(data.expires_in) * 1000,
