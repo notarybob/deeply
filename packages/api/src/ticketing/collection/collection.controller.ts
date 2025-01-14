efd3106c0051b4e0d99aaa3d02d7f7d12c340dd0
notarybob <serjoksalot@yahoo.com>
2025-01-14 04:00:06 +0000

    Optimize API - enhancing logs

--- packages/api/src/ticketing/collection/collection.controller.ts
+++ packages/api/src/ticketing/collection/collection.controller.ts
@@ -66,11 +66,11 @@ export class CollectionController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.collectionService.getCollections(
         connectionId,
         projectId,
@@ -119,7 +119,7 @@ export class CollectionController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
