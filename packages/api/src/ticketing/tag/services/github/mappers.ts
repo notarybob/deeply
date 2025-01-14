99affcc618dce7c467f7af0eb69b8f840a37fa92
notarybob <serjoksalot@yahoo.com>
2025-01-14 13:14:39 +0000

    Add tests - fixing a bug

--- packages/api/src/ticketing/tag/services/github/mappers.ts
+++ packages/api/src/ticketing/tag/services/github/mappers.ts
@@ -32,7 +32,7 @@ export class GithubTagMapper implements ITagMapper {
         }[],
     ): UnifiedTicketingTagOutput | UnifiedTicketingTagOutput[] {
         // If the source is not an array, convert it to an array for mapping
-        const sourcesArray = Array.isArray(source) ? source : [source];
+        let sourcesArray = Array.isArray(source) ? source : [source];
 
         return sourcesArray.map((tag) =>
             this.mapSingleTagToUnified(tag, connectionId, customFieldMappings),
@@ -47,7 +47,7 @@ export class GithubTagMapper implements ITagMapper {
             remote_id: string;
         }[],
     ): UnifiedTicketingTagOutput {
-        const unifiedTag: UnifiedTicketingTagOutput = {
+        let unifiedTag: UnifiedTicketingTagOutput = {
             remote_id: tag.id ? String(tag.id) : null,
             remote_data: tag,
             name: tag.name,
