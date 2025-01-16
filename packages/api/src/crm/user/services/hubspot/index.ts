9b88d92013d11513dd22d43692a7ff42ef8763c0
notarybob <serjoksalot@yahoo.com>
2025-01-16 15:59:57 +0000

    Remove API - improving performance

--- packages/api/src/crm/user/services/hubspot/index.ts
+++ packages/api/src/crm/user/services/hubspot/index.ts
@@ -26,9 +26,9 @@ export class HubspotService implements IUserService {
 
   async sync(data: SyncParam): Promise<ApiResponse<HubspotUserOutput[]>> {
     try {
-      const { linkedUserId, custom_properties } = data;
+      let { linkedUserId, custom_properties } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'hubspot',
@@ -36,17 +36,17 @@ export class HubspotService implements IUserService {
         },
       });
 
-      const commonPropertyNames = Object.keys(commonUserHubspotProperties);
-      const allProperties = [...commonPropertyNames, ...custom_properties];
-      const baseURL = `${connection.account_url}/crm/v3/owners`;
+      let commonPropertyNames = Object.keys(commonUserHubspotProperties);
+      let allProperties = [...commonPropertyNames, ...custom_properties];
+      let baseURL = `${connection.account_url}/crm/v3/owners`;
 
-      /*const queryString = allProperties
+      /*let queryString = allProperties
         .map((prop) => `properties=${encodeURIComponent(prop)}`)
         .join('&');*/
 
-      //const url = `${baseURL}?${queryString}`;
+      //let url = `${baseURL}?${queryString}`;
 
-      const resp = await axios.get(baseURL, {
+      let resp = await axios.get(baseURL, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
