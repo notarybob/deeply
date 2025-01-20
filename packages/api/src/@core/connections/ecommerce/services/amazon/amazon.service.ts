5432f5652f84500ec2d0e4e4655d792bef7d355e
notarybob <serjoksalot@yahoo.com>
2025-01-20 21:15:12 +0000

    Remove README - updating docs

--- packages/api/src/@core/connections/ecommerce/services/amazon/amazon.service.ts
+++ packages/api/src/@core/connections/ecommerce/services/amazon/amazon.service.ts
@@ -55,16 +55,16 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
       });
 
-      var access_token = JSON.parse(
+      const access_token = JSON.parse(
         this.cryptoService.decrypt(connection.access_token),
       );
       config.headers = {
@@ -90,8 +90,8 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, spapi_oauth_code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, spapi_oauth_code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'amazon',
@@ -99,25 +99,25 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
         },
       });
       //reconstruct the redirect URI that was passed in the frontend it must be the same
-      var REDIRECT_URI = `${
+      const REDIRECT_URI = `${
         this.env.getDistributionMode() == 'selfhost'
           ? this.env.getTunnelIngress()
           : this.env.getPanoraBaseUrl()
       }/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'authorization_code',
         redirect_uri: REDIRECT_URI,
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         code: spapi_oauth_code,
       });
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.amazon.com/auth/o2/token',
         formData.toString(),
         {
@@ -126,10 +126,10 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: AmazonOAuthResponse = res.data;
+      const data: AmazonOAuthResponse = res.data;
       // save tokens for this customer inside our db
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         // Update existing connection
@@ -190,20 +190,20 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.amazon.com/auth/o2/token',
         formData.toString(),
         {
@@ -212,8 +212,8 @@ export class AmazonConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: AmazonOAuthResponse = res.data;
-      var res_ = await this.prisma.connections.update({
+      const data: AmazonOAuthResponse = res.data;
+      const res_ = await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
         },
