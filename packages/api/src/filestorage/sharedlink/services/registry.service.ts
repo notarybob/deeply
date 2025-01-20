f2da0966f7cde8b6ff33bd932ee4275c8960f880
notarybob <serjoksalot@yahoo.com>
2025-01-20 02:54:00 +0000

    Document configuration - updating docs

--- packages/api/src/filestorage/sharedlink/services/registry.service.ts
+++ packages/api/src/filestorage/sharedlink/services/registry.service.ts
@@ -14,7 +14,7 @@ export class ServiceRegistry {
   }
 
   getService(integrationId: string): ISharedLinkService {
-    let service = this.serviceMap.get(integrationId);
+    const service = this.serviceMap.get(integrationId);
     if (!service) {
       //throw new ReferenceError();
       return null;
