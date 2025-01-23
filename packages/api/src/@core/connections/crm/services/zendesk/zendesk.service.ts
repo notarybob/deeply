d1846d22e7e776f369f91970aa6e59befa8f7e67
notarybob <serjoksalot@yahoo.com>
2025-01-23 15:58:23 +0000

    Remove auth - improving performance

--- packages/api/src/@core/connections/crm/services/zendesk/zendesk.service.ts
+++ packages/api/src/@core/connections/crm/services/zendesk/zendesk.service.ts
@@ -56,10 +56,10 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -91,8 +91,8 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'zendesk',
@@ -100,18 +100,18 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'authorization_code',
         redirect_uri: REDIRECT_URI,
         code: code,
       });
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.getbase.com/oauth2/token',
         formData.toString(),
         {
@@ -123,11 +123,11 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: ZendeskSellOAuthResponse = res.data;
+      const data: ZendeskSellOAuthResponse = res.data;
       this.logger.log('OAuth credentials : zendesk ' + JSON.stringify(data));
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -186,17 +186,17 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
   }
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var formData = new URLSearchParams({
+      const { connectionId, refreshToken, projectId } = opts;
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.getbase.com/oauth2/token',
         formData.toString(),
         {
@@ -208,7 +208,7 @@ export class ZendeskConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: ZendeskSellOAuthResponse = res.data;
+      const data: ZendeskSellOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
