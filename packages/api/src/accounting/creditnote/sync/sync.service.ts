5757fc27f9d49d48024a74cfd886fd61c9e5f967
notarybob <serjoksalot@yahoo.com>
2025-01-27 02:52:50 +0000

    Update styles - fixing a bug

--- packages/api/src/accounting/creditnote/sync/sync.service.ts
+++ packages/api/src/accounting/creditnote/sync/sync.service.ts
@@ -39,15 +39,15 @@ export class SyncService implements OnModuleInit, IBaseSync {
   @Cron('0 */8 * * *') // every 8 hours
   async kickstartSync(id_project?: string) {
     try {
-      var linkedUsers = await this.prisma.linked_users.findMany({
+      const linkedUsers = await this.prisma.linked_users.findMany({
         where: {
           id_project: id_project,
         },
       });
       linkedUsers.map(async (linkedUser) => {
         try {
-          var providers = ACCOUNTING_PROVIDERS;
-          for (var provider of providers) {
+          const providers = ACCOUNTING_PROVIDERS;
+          for (const provider of providers) {
             try {
               await this.syncForLinkedUser({
                 integrationId: provider,
@@ -68,8 +68,8 @@ export class SyncService implements OnModuleInit, IBaseSync {
 
   async syncForLinkedUser(param: SyncLinkedUserType) {
     try {
-      var { integrationId, linkedUserId } = param;
-      var service: ICreditNoteService =
+      const { integrationId, linkedUserId } = param;
+      const service: ICreditNoteService =
         this.serviceRegistry.getService(integrationId);
       if (!service) return;
 
@@ -91,11 +91,11 @@ export class SyncService implements OnModuleInit, IBaseSync {
     remote_data: Record<string, any>[],
   ): Promise<AccCreditNote[]> {
     try {
-      var creditNoteResults: AccCreditNote[] = [];
+      const creditNoteResults: AccCreditNote[] = [];
 
       for (let i = 0; i < creditNotes.length; i++) {
-        var creditNote = creditNotes[i];
-        var originId = creditNote.remote_id;
+        const creditNote = creditNotes[i];
+        const originId = creditNote.remote_id;
 
         let existingCreditNote = await this.prisma.acc_credit_notes.findFirst({
           where: {
@@ -104,7 +104,7 @@ export class SyncService implements OnModuleInit, IBaseSync {
           },
         });
 
-        var creditNoteData = {
+        const creditNoteData = {
           transaction_date: creditNote.transaction_date
             ? new Date(creditNote.transaction_date)
             : null,
