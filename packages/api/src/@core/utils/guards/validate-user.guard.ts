b439a690131b8c2234b59abebcbf2533c22bd69a
notarybob <serjoksalot@yahoo.com>
2025-01-27 22:41:17 +0000

    Refactor API - improving security

--- packages/api/src/@core/utils/guards/validate-user.guard.ts
+++ packages/api/src/@core/utils/guards/validate-user.guard.ts
@@ -10,9 +10,9 @@ export class ValidateUserGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const { id_user } = request.user;
-    const project_id = request.query.project_id; // Extract project_id from the query parameters
+    let request = context.switchToHttp().getRequest();
+    let { id_user } = request.user;
+    let project_id = request.query.project_id; // Extract project_id from the query parameters
 
     // Ensure project_id is provided and is a string
     if (!project_id || typeof project_id !== 'string') {
