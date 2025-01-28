21dbe0cc6e66f221dcb3c5e2a1fac77186589ad2
notarybob <serjoksalot@yahoo.com>
2025-01-28 12:49:01 +0000

    Refactor configuration - fixing a bug

--- packages/api/src/ticketing/team/services/front/index.ts
+++ packages/api/src/ticketing/team/services/front/index.ts
@@ -27,9 +27,9 @@ export class FrontService implements ITeamService {
 
   async sync(data: SyncParam): Promise<ApiResponse<FrontTeamOutput[]>> {
     try {
-      let { linkedUserId } = data;
+      const { linkedUserId } = data;
 
-      let connection = await this.prisma.connections.findFirst({
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'front',
@@ -37,7 +37,7 @@ export class FrontService implements ITeamService {
         },
       });
 
-      let resp = await axios.get(`${connection.account_url}/teams`, {
+      const resp = await axios.get(`${connection.account_url}/teams`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
