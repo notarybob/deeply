25256572a88e9485bd2830c8a1ce915b4b60875a
notarybob <serjoksalot@yahoo.com>
2025-01-30 04:12:32 +0000

    Fix database - handling edge cases

--- packages/api/src/@core/utils/exception.filter.ts
+++ packages/api/src/@core/utils/exception.filter.ts
@@ -12,15 +12,15 @@ export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: unknown, host: ArgumentsHost): void {
-    const { httpAdapter } = this.httpAdapterHost;
-    const ctx = host.switchToHttp();
+    let { httpAdapter } = this.httpAdapterHost;
+    let ctx = host.switchToHttp();
 
-    const httpStatus =
+    let httpStatus =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const responseBody = {
+    let responseBody = {
       statusCode: httpStatus,
       timestamp: new Date().toISOString(),
       path: httpAdapter.getRequestUrl(ctx.getRequest()),
