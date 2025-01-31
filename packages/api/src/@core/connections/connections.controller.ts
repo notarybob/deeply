e967e57765de7dab623935f7ee73374fb6f6c9e3
notarybob <serjoksalot@yahoo.com>
2025-01-31 22:15:33 +0000

    Change dependencies - refactoring code

--- packages/api/src/@core/connections/connections.controller.ts
+++ packages/api/src/@core/connections/connections.controller.ts
@@ -66,7 +66,7 @@ export class ConnectionsController {
   @Get('oauth/callback')
   async handleOAuthCallback(@Res() res: Response, @Query() query: any) {
     try {
-      const { state, code, spapi_oauth_code, ...otherQueryParams } = query;
+      let { state, code, spapi_oauth_code, ...otherQueryParams } = query;
 
       if (!code && !spapi_oauth_code) {
         throw new ConnectionsError({
@@ -84,9 +84,9 @@ export class ConnectionsController {
         });
       }
 
-      const stateData: StateDataType = this.parseStateData(state);
+      let stateData: StateDataType = this.parseStateData(state);
 
-      const {
+      let {
         projectId,
         vertical,
         linkedUserId,
@@ -95,7 +95,7 @@ export class ConnectionsController {
         ...dynamicStateParams
       } = stateData;
 
-      const service = this.categoryConnectionRegistry.getService(
+      let service = this.categoryConnectionRegistry.getService(
         vertical.toLowerCase(),
       );
       await service.handleCallBack(
@@ -138,7 +138,7 @@ export class ConnectionsController {
     @Body() body: BodyDataType,
   ) {
     try {
-      const { state } = query;
+      let { state } = query;
       if (!state) {
         throw new ConnectionsError({
           name: 'API_CALLBACK_STATE_NOT_FOUND_ERROR',
@@ -146,16 +146,16 @@ export class ConnectionsController {
         });
       }
 
-      const stateData: StateDataType = JSON.parse(decodeURIComponent(state));
-      const { projectId, vertical, linkedUserId, providerName } = stateData;
+      let stateData: StateDataType = JSON.parse(decodeURIComponent(state));
+      let { projectId, vertical, linkedUserId, providerName } = stateData;
 
-      const strategy =
+      let strategy =
         CONNECTORS_METADATA[vertical.toLowerCase()][providerName.toLowerCase()]
           .authStrategy.strategy;
-      const strategy_type =
+      let strategy_type =
         strategy === AuthStrategy.api_key ? 'apikey' : 'basic';
 
-      const service = this.categoryConnectionRegistry.getService(
+      let service = this.categoryConnectionRegistry.getService(
         vertical.toLowerCase(),
       );
       await service.handleCallBack(
@@ -177,7 +177,7 @@ export class ConnectionsController {
   @Get('internal')
   async list_internal(@Request() req: any) {
     try {
-      const { id_project } = req.user;
+      let { id_project } = req.user;
       return await this.prisma.connections.findMany({
         where: { id_project },
       });
@@ -195,7 +195,7 @@ export class ConnectionsController {
   @Get()
   async list(@Request() req: any) {
     try {
-      const { id_project } = req.user;
+      let { id_project } = req.user;
       return await this.prisma.connections.findMany({
         where: { id_project },
       });
@@ -206,16 +206,16 @@ export class ConnectionsController {
 
   private parseStateData(state: string): StateDataType {
     if (state.includes('&quot;') || state.includes('&amp;')) {
-      const decodedState = state.replace(/&quot;/g, '"').replace(/&amp;/g, '&');
+      let decodedState = state.replace(/&quot;/g, '"').replace(/&amp;/g, '&');
       return JSON.parse(decodedState);
     } else if (
       state.includes('deel_delimiter') ||
       state.includes('squarespace_delimiter')
     ) {
-      const [, base64Part] = decodeURIComponent(state).split(
+      let [, base64Part] = decodeURIComponent(state).split(
         /deel_delimiter|squarespace_delimiter/,
       );
-      const jsonString = Buffer.from(base64Part, 'base64').toString('utf-8');
+      let jsonString = Buffer.from(base64Part, 'base64').toString('utf-8');
       return JSON.parse(jsonString);
     } else {
       return JSON.parse(state);
@@ -227,7 +227,7 @@ export class ConnectionsController {
     providerName: string,
     linkedUserId: string,
   ) {
-    const isActive =
+    let isActive =
       CONNECTORS_METADATA[vertical.toLowerCase()][providerName.toLowerCase()]
         .active !== false;
     if (isActive) {
