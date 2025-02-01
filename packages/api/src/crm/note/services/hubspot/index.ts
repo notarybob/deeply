be6e7a13e67df93af7218fd2a6fc9352950c9567
notarybob <serjoksalot@yahoo.com>
2025-02-01 11:08:35 +0000

    Change database - improving performance

--- packages/api/src/crm/note/services/hubspot/index.ts
+++ packages/api/src/crm/note/services/hubspot/index.ts
@@ -32,14 +32,14 @@ export class HubspotService implements INoteService {
     linkedUserId: string,
   ): Promise<ApiResponse<HubspotNoteOutput>> {
     try {
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'hubspot',
           vertical: 'crm',
         },
       });
-      const resp = await axios.post(
+      let resp = await axios.post(
         `${connection.account_url}/crm/v3/objects/notes`,
         JSON.stringify(noteData),
         {
@@ -52,7 +52,7 @@ export class HubspotService implements INoteService {
         },
       );
 
-      const final_resp = await axios.get(
+      let final_resp = await axios.get(
         `${connection.account_url}/crm/v3/objects/notes/${resp.data.id}?properties=hs_note_body&associations=deal,contact,company`,
         {
           headers: {
@@ -75,9 +75,9 @@ export class HubspotService implements INoteService {
 
   async sync(data: SyncParam): Promise<ApiResponse<HubspotNoteOutput[]>> {
     try {
-      const { linkedUserId, custom_properties } = data;
+      let { linkedUserId, custom_properties } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'hubspot',
@@ -85,17 +85,17 @@ export class HubspotService implements INoteService {
         },
       });
 
-      const commonPropertyNames = Object.keys(commonNoteHubspotProperties);
-      const allProperties = [...commonPropertyNames, ...custom_properties];
-      const baseURL = `${connection.account_url}/crm/v3/objects/notes`;
+      let commonPropertyNames = Object.keys(commonNoteHubspotProperties);
+      let allProperties = [...commonPropertyNames, ...custom_properties];
+      let baseURL = `${connection.account_url}/crm/v3/objects/notes`;
 
-      const queryString = allProperties
+      let queryString = allProperties
         .map((prop) => `properties=${encodeURIComponent(prop)}`)
         .join('&');
 
-      const url = `${baseURL}?${queryString}&associations=deal,contact,company`;
+      let url = `${baseURL}?${queryString}&associations=deal,contact,company`;
 
-      const resp = await axios.get(url, {
+      let resp = await axios.get(url, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
