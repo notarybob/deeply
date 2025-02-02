9232cf54ead130cf10f1d9dd29b88f57e5127c37
notarybob <serjoksalot@yahoo.com>
2025-02-02 15:12:15 +0000

    Change styles - refactoring code

--- packages/api/src/@core/connections/crm/services/wealthbox/wealthbox.service.ts
+++ packages/api/src/@core/connections/crm/services/wealthbox/wealthbox.service.ts
@@ -58,10 +58,10 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -93,8 +93,8 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, code } = opts;
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var { linkedUserId, projectId, code } = opts;
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'wealthbox',
@@ -102,21 +102,21 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
         },
       });
       //reconstruct the redirect URI that was passed in the frontend it must be the same
-      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
 
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         grant_type: 'authorization_code',
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
       });
-      const res = await axios.post(
+      var res = await axios.post(
         'https://app.crmworkspace.com/oauth/token',
         formData.toString(),
         {
@@ -125,10 +125,10 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: WealthboxOAuthResponse = res.data;
+      var data: WealthboxOAuthResponse = res.data;
       // save tokens for this customer inside our db
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
 
       if (isNotUnique) {
         // Update existing connection
@@ -187,20 +187,20 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      const { connectionId, refreshToken, projectId } = opts;
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var { connectionId, refreshToken, projectId } = opts;
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const params = new URLSearchParams({
+      var params = new URLSearchParams({
         grant_type: 'refresh_token',
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
 
-      const res = await axios.post(
+      var res = await axios.post(
         'https://app.crmworkspace.com/oauth/token',
         params.toString(),
         {
@@ -209,8 +209,8 @@ export class WealthboxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: WealthboxOAuthResponse = res.data;
-      const res_ = await this.prisma.connections.update({
+      var data: WealthboxOAuthResponse = res.data;
+      var res_ = await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
         },
