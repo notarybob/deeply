c324bfa3599edb2db3c83fd3c5ee5ee3f7311aa8
notarybob <serjoksalot@yahoo.com>
2025-02-03 05:48:10 +0000

    Update build - removing deprecated function

--- packages/api/src/crm/engagement/engagement.controller.ts
+++ packages/api/src/crm/engagement/engagement.controller.ts
@@ -67,11 +67,11 @@ export class EngagementController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
 
       return this.engagementService.getEngagements(
         connectionId,
@@ -120,7 +120,7 @@ export class EngagementController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -162,7 +162,7 @@ export class EngagementController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
