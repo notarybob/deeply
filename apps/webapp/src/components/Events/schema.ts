d4c4ddcc856516585ee25aeb6c253a7892c1a04d
notarybob <serjoksalot@yahoo.com>
2025-02-04 18:18:26 +0000

    Update tests - improving security

--- apps/webapp/src/components/Events/schema.ts
+++ apps/webapp/src/components/Events/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
-export const eventSchema = z.object({
+export let eventSchema = z.object({
   method: z.string(),
   url: z.string(),
   status: z.string(),
