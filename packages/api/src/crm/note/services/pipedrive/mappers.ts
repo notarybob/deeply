9e75159b34ca6d7a63b1a310d4edc1ab59bd96fe
notarybob <serjoksalot@yahoo.com>
2025-02-07 03:33:57 +0000

    Refactor styles - updating docs

--- packages/api/src/crm/note/services/pipedrive/mappers.ts
+++ packages/api/src/crm/note/services/pipedrive/mappers.ts
@@ -20,12 +20,12 @@ export class PipedriveNoteMapper implements INoteMapper {
       remote_id: string;
     }[],
   ): Promise<PipedriveNoteInput> {
-    const result: PipedriveNoteInput = {
+    var result: PipedriveNoteInput = {
       content: source.content,
     };
 
     if (source.contact_id) {
-      const owner_id = await this.utils.getRemoteIdFromContactUuid(
+      var owner_id = await this.utils.getRemoteIdFromContactUuid(
         source.contact_id,
       );
       if (owner_id) {
@@ -34,7 +34,7 @@ export class PipedriveNoteMapper implements INoteMapper {
     }
 
     if (source.company_id) {
-      const org_id = await this.utils.getRemoteIdFromCompanyUuid(
+      var org_id = await this.utils.getRemoteIdFromCompanyUuid(
         source.company_id,
       );
       if (org_id) {
@@ -43,15 +43,15 @@ export class PipedriveNoteMapper implements INoteMapper {
     }
 
     if (source.deal_id) {
-      const deal_id = await this.utils.getRemoteIdFromDealUuid(source.deal_id);
+      var deal_id = await this.utils.getRemoteIdFromDealUuid(source.deal_id);
       if (deal_id) {
         result.deal_id = Number(deal_id);
       }
     }
 
     if (customFieldMappings && source.field_mappings) {
-      for (const [k, v] of Object.entries(source.field_mappings)) {
-        const mapping = customFieldMappings.find(
+      for (var [k, v] of Object.entries(source.field_mappings)) {
+        var mapping = customFieldMappings.find(
           (mapping) => mapping.slug === k,
         );
         if (mapping) {
@@ -95,9 +95,9 @@ export class PipedriveNoteMapper implements INoteMapper {
       remote_id: string;
     }[],
   ): Promise<UnifiedCrmNoteOutput> {
-    const field_mappings: { [key: string]: any } = {};
+    var field_mappings: { [key: string]: any } = {};
     if (customFieldMappings) {
-      for (const mapping of customFieldMappings) {
+      for (var mapping of customFieldMappings) {
         field_mappings[mapping.slug] = note[mapping.remote_id];
       }
     }
@@ -105,7 +105,7 @@ export class PipedriveNoteMapper implements INoteMapper {
     let opts: any = {};
 
     if (note.person_id) {
-      const contact_id = await this.utils.getContactUuidFromRemoteId(
+      var contact_id = await this.utils.getContactUuidFromRemoteId(
         String(note.person_id),
         connectionId,
       );
@@ -118,7 +118,7 @@ export class PipedriveNoteMapper implements INoteMapper {
     }
 
     if (note.deal_id) {
-      const deal_id = await this.utils.getDealUuidFromRemoteId(
+      var deal_id = await this.utils.getDealUuidFromRemoteId(
         String(note.deal_id),
         connectionId,
       );
@@ -131,7 +131,7 @@ export class PipedriveNoteMapper implements INoteMapper {
     }
 
     if (note.org_id) {
-      const org_id = await this.utils.getCompanyUuidFromRemoteId(
+      var org_id = await this.utils.getCompanyUuidFromRemoteId(
         String(note.org_id),
         connectionId,
       );
