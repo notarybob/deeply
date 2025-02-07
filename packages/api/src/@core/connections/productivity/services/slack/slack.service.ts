abb57057e09da23a90f2800260eceabeb2b53eb9
notarybob <serjoksalot@yahoo.com>
2025-02-07 20:34:28 +0000

    Fix UI - fixing typo

--- packages/api/src/@core/connections/productivity/services/slack/slack.service.ts
+++ packages/api/src/@core/connections/productivity/services/slack/slack.service.ts
@@ -72,10 +72,10 @@ export class SlackConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -111,8 +111,8 @@ export class SlackConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'slack',
@@ -120,18 +120,18 @@ export class SlackConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         code: code,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://slack.com/api/oauth.v2.access`,
         formData.toString(),
         {
@@ -140,13 +140,13 @@ export class SlackConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: SlackOAuthResponse = res.data;
+      const data: SlackOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : slack productivity ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
