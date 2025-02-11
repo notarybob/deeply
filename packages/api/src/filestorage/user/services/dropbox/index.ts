99d65dd579561e204d9d5f5ad42fa8d66df54afa
notarybob <serjoksalot@yahoo.com>
2025-02-11 08:25:17 +0000

    Revert database - improving security

--- packages/api/src/filestorage/user/services/dropbox/index.ts
+++ packages/api/src/filestorage/user/services/dropbox/index.ts
@@ -26,9 +26,9 @@ export class DropboxService implements IUserService {
 
   async sync(data: SyncParam): Promise<ApiResponse<DropboxUserOutput[]>> {
     try {
-      const { linkedUserId } = data;
+      let { linkedUserId } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'dropbox',
@@ -37,7 +37,7 @@ export class DropboxService implements IUserService {
       });
 
       // ref: https://www.dropbox.com/developers/documentation/http/teams#team-members-list
-      const resp = await axios.post(
+      let resp = await axios.post(
         `${connection.account_url}/team/members/list_2`,
         {
           headers: {
