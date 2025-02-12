c807c1fc721edc3b4eadb4b42161441b9772aa60
notarybob <serjoksalot@yahoo.com>
2025-02-12 18:19:45 +0000

    Add dependencies - removing deprecated function

--- packages/api/src/marketingautomation/user/user.controller.ts
+++ packages/api/src/marketingautomation/user/user.controller.ts
@@ -63,11 +63,11 @@ export class UserController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.userService.getUsers(
         connectionId,
         projectId,
@@ -117,7 +117,7 @@ export class UserController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
