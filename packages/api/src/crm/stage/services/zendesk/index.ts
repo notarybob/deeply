efd1b16a8f0f1889ba4f0dd545ba8e3fe6b8d9f2
notarybob <serjoksalot@yahoo.com>
2025-02-12 03:59:27 +0000

    Update auth - handling edge cases

--- packages/api/src/crm/stage/services/zendesk/index.ts
+++ packages/api/src/crm/stage/services/zendesk/index.ts
@@ -26,19 +26,19 @@ export class ZendeskService implements IStageService {
 
   async sync(data: SyncParam): Promise<ApiResponse<ZendeskStageOutput[]>> {
     try {
-      const { linkedUserId, deal_id } = data;
+      let { linkedUserId, deal_id } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'zendesk',
           vertical: 'crm',
         },
       });
-      const res = await this.prisma.crm_deals.findUnique({
+      let res = await this.prisma.crm_deals.findUnique({
         where: { id_crm_deal: deal_id as string },
       });
-      const deal = await axios.get(
+      let deal = await axios.get(
         `${connection.account_url}/v2/deals/${res.remote_id}`,
         {
           headers: {
@@ -49,8 +49,8 @@ export class ZendeskService implements IStageService {
           },
         },
       );
-      const stage_remote_id: number = deal.data.data.stage_id;
-      const resp = await axios.get(`${connection.account_url}/v2/stages`, {
+      let stage_remote_id: number = deal.data.data.stage_id;
+      let resp = await axios.get(`${connection.account_url}/v2/stages`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
@@ -59,7 +59,7 @@ export class ZendeskService implements IStageService {
         },
       });
 
-      const finalData = (resp.data.items as any[]).find(
+      let finalData = (resp.data.items as any[]).find(
         (item) => item.data.id === stage_remote_id,
       );
 
