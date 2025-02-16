95454f5567f4fc432d1bd1b64eba865ff0194c1d
notarybob <serjoksalot@yahoo.com>
2025-02-16 14:02:16 +0000

    Add API - removing deprecated function

--- packages/api/src/accounting/purchaseorder/purchaseorder.controller.ts
+++ packages/api/src/accounting/purchaseorder/purchaseorder.controller.ts
@@ -66,11 +66,11 @@ export class PurchaseOrderController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.purchaseorderService.getPurchaseOrders(
         connectionId,
         projectId,
@@ -120,7 +120,7 @@ export class PurchaseOrderController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -163,7 +163,7 @@ export class PurchaseOrderController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
