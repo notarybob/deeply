4aa880c3f31056f886ee9a0dc1e3db056110a9b6
notarybob <serjoksalot@yahoo.com>
2025-02-17 13:40:03 +0000

    Remove database - removing deprecated function

--- packages/api/src/crm/user/services/pipedrive/mappers.ts
+++ packages/api/src/crm/user/services/pipedrive/mappers.ts
@@ -20,14 +20,14 @@ export class PipedriveUserMapper implements IUserMapper {
       remote_id: string;
     }[],
   ): PipedriveUserInput {
-    let result: PipedriveUserInput = {
+    const result: PipedriveUserInput = {
       name: source.name,
       email: source.email,
     };
 
     if (customFieldMappings && source.field_mappings) {
-      for (let [k, v] of Object.entries(source.field_mappings)) {
-        let mapping = customFieldMappings.find(
+      for (const [k, v] of Object.entries(source.field_mappings)) {
+        const mapping = customFieldMappings.find(
           (mapping) => mapping.slug === k,
         );
         if (mapping) {
@@ -71,9 +71,9 @@ export class PipedriveUserMapper implements IUserMapper {
       remote_id: string;
     }[],
   ): UnifiedCrmUserOutput {
-    let field_mappings: { [key: string]: any } = {};
+    const field_mappings: { [key: string]: any } = {};
     if (customFieldMappings) {
-      for (let mapping of customFieldMappings) {
+      for (const mapping of customFieldMappings) {
         field_mappings[mapping.slug] = user[mapping.remote_id];
       }
     }
