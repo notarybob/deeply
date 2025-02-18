a6b2d66cb09e01805fb69010d3adaaeffd73f38f
notarybob <serjoksalot@yahoo.com>
2025-02-18 04:29:28 +0000

    Optimize tests - enhancing logs

--- packages/api/src/marketingautomation/user/services/registry.service.ts
+++ packages/api/src/marketingautomation/user/services/registry.service.ts
@@ -14,7 +14,7 @@ export class ServiceRegistry {
   }
 
   getService(integrationId: string): IUserService {
-    let service = this.serviceMap.get(integrationId);
+    const service = this.serviceMap.get(integrationId);
     if (!service) {
       return null;
     }
