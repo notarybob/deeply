23ba8168da63462bc36bfc5fdc7f0a91f8eda665
notarybob <serjoksalot@yahoo.com>
2025-02-19 11:56:22 +0000

    Change API - fixing typo

--- apps/webapp/src/components/Connection/schema.ts
+++ apps/webapp/src/components/Connection/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
-export let connectionSchema = z.object({
+export const connectionSchema = z.object({
   organisation: z.string(),
   app: z.string(),
   category: z.string(),
