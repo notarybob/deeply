cdd4c2763209db3a9a07424222cb4ab7866eb9f2
notarybob <serjoksalot@yahoo.com>
2025-02-20 06:34:35 +0000

    Update README - fixing typo

--- packages/api/src/crm/company/services/pipedrive/index.ts
+++ packages/api/src/crm/company/services/pipedrive/index.ts
@@ -29,14 +29,14 @@ export class PipedriveService implements ICompanyService {
     linkedUserId: string,
   ): Promise<ApiResponse<PipedriveCompanyOutput>> {
     try {
-      let connection = await this.prisma.connections.findFirst({
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'pipedrive',
           vertical: 'crm',
         },
       });
-      let resp = await axios.post(
+      const resp = await axios.post(
         `${connection.account_url}/v1/organizations`,
         JSON.stringify(companyData),
         {
@@ -60,15 +60,15 @@ export class PipedriveService implements ICompanyService {
 
   async sync(data: SyncParam): Promise<ApiResponse<PipedriveCompanyOutput[]>> {
     try {
-      let { linkedUserId } = data;
-      let connection = await this.prisma.connections.findFirst({
+      const { linkedUserId } = data;
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'pipedrive',
           vertical: 'crm',
         },
       });
-      let resp = await axios.get(
+      const resp = await axios.get(
         `${connection.account_url}/v1/organizations`,
         {
           headers: {
