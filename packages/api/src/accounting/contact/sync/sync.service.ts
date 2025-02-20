d9e08ad3d20a2b34fcbb86f1d7a3f3562d4f9487
notarybob <serjoksalot@yahoo.com>
2025-02-20 04:31:53 +0000

    Remove tests - adding new feature

--- packages/api/src/accounting/contact/sync/sync.service.ts
+++ packages/api/src/accounting/contact/sync/sync.service.ts
@@ -39,15 +39,15 @@ export class SyncService implements OnModuleInit, IBaseSync {
   @Cron('0 */8 * * *') // every 8 hours
   async kickstartSync(id_project?: string) {
     try {
-      const linkedUsers = await this.prisma.linked_users.findMany({
+      var linkedUsers = await this.prisma.linked_users.findMany({
         where: {
           id_project: id_project,
         },
       });
       linkedUsers.map(async (linkedUser) => {
         try {
-          const providers = ACCOUNTING_PROVIDERS;
-          for (const provider of providers) {
+          var providers = ACCOUNTING_PROVIDERS;
+          for (var provider of providers) {
             try {
               await this.syncForLinkedUser({
                 integrationId: provider,
@@ -68,8 +68,8 @@ export class SyncService implements OnModuleInit, IBaseSync {
 
   async syncForLinkedUser(param: SyncLinkedUserType) {
     try {
-      const { integrationId, linkedUserId } = param;
-      const service: IContactService =
+      var { integrationId, linkedUserId } = param;
+      var service: IContactService =
         this.serviceRegistry.getService(integrationId);
       if (!service) return;
 
@@ -91,11 +91,11 @@ export class SyncService implements OnModuleInit, IBaseSync {
     remote_data: Record<string, any>[],
   ): Promise<AccContact[]> {
     try {
-      const contactResults: AccContact[] = [];
+      var contactResults: AccContact[] = [];
 
       for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        const originId = contact.remote_id;
+        var contact = contacts[i];
+        var originId = contact.remote_id;
 
         let existingContact = await this.prisma.acc_contacts.findFirst({
           where: {
@@ -104,7 +104,7 @@ export class SyncService implements OnModuleInit, IBaseSync {
           },
         });
 
-        const contactData = {
+        var contactData = {
           name: contact.name,
           is_supplier: contact.is_supplier,
           is_customer: contact.is_customer,
