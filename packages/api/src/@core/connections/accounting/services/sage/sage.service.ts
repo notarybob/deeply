f055a3293dfb781c95cba9c08d8824bebd740913
notarybob <serjoksalot@yahoo.com>
2025-02-21 09:03:21 +0000

    Fix styles - fixing typo

--- packages/api/src/@core/connections/accounting/services/sage/sage.service.ts
+++ packages/api/src/@core/connections/accounting/services/sage/sage.service.ts
@@ -58,10 +58,10 @@ export class SageConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -93,8 +93,8 @@ export class SageConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'sage',
@@ -103,20 +103,20 @@ export class SageConnectionService extends AbstractBaseConnectionService {
       });
 
       //reconstruct the redirect URI that was passed in the githubend it must be the same
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         'https://oauth.accounting.sage.com/token',
         formData.toString(),
         {
@@ -125,13 +125,13 @@ export class SageConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: SageOAuthResponse = res.data;
+      const data: SageOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : sage accounting ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -189,20 +189,20 @@ export class SageConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         'https://oauth.accounting.sage.com/toke',
         formData.toString(),
         {
@@ -211,7 +211,7 @@ export class SageConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: SageOAuthResponse = res.data;
+      const data: SageOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
