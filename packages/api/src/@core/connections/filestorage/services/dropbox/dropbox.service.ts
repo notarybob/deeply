d58467ee7dc31090e10e6e3540a9f872be2044ef
notarybob <serjoksalot@yahoo.com>
2025-02-22 20:11:29 +0000

    Refactor build - enhancing logs

--- packages/api/src/@core/connections/filestorage/services/dropbox/dropbox.service.ts
+++ packages/api/src/@core/connections/filestorage/services/dropbox/dropbox.service.ts
@@ -56,10 +56,10 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -91,8 +91,8 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'dropbox',
@@ -100,21 +100,21 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         redirect_uri: REDIRECT_URI,
         code: code,
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://api.dropbox.com/oauth2/token`,
         formData.toString(),
         {
@@ -123,13 +123,13 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: DropboxOAuthResponse = res.data;
+      const data: DropboxOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : dropbox filestorage ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -186,15 +186,15 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
   }
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const { connectionId, refreshToken, projectId } = opts;
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
         client_id: CREDENTIALS.CLIENT_ID,
@@ -202,7 +202,7 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
         redirect_uri: REDIRECT_URI,
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         `https://api.dropbox.com/oauth2/token`,
         formData.toString(),
         {
@@ -211,7 +211,7 @@ export class DropboxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: DropboxOAuthResponse = res.data;
+      const data: DropboxOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
