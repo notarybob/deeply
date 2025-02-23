9b6cb8bd24d7d7246cc10218cef0178261e165db
notarybob <serjoksalot@yahoo.com>
2025-02-23 01:37:14 +0000

    Remove database - removing deprecated function

--- packages/api/src/crm/stage/stage.controller.ts
+++ packages/api/src/crm/stage/stage.controller.ts
@@ -59,11 +59,11 @@ export class StageController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.stageService.getStages(
         connectionId,
         projectId,
@@ -111,7 +111,7 @@ export class StageController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
