50fd2cdaa00416408d224a154a65a32584bd73c1
notarybob <serjoksalot@yahoo.com>
2025-02-24 04:53:40 +0000

    Remove database - removing deprecated function

--- packages/api/src/@core/connections/ticketing/services/ironclad/ironclad.service.ts
+++ packages/api/src/@core/connections/ticketing/services/ironclad/ironclad.service.ts
@@ -56,10 +56,10 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -91,8 +91,8 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'ironclad',
@@ -100,25 +100,25 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var REDIRECT_URI = `${
+      const REDIRECT_URI = `${
         this.env.getDistributionMode() == 'selfhost'
           ? this.env.getTunnelIngress()
           : this.env.getPanoraBaseUrl()
       }/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = {
+      const formData = {
         grant_type: 'authorization_code',
         redirect_uri: REDIRECT_URI,
         code: code,
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
       };
-      var res = await axios.post(
+      const res = await axios.post(
         `https://ironcladapp.com/oauth/token`,
         JSON.stringify(formData),
         {
@@ -127,13 +127,13 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: IroncladOAuthResponse = res.data;
+      const data: IroncladOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : ironclad ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -191,19 +191,19 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         `https://ironcladapp.com/oauth/token`,
         formData.toString(),
         {
@@ -212,7 +212,7 @@ export class IroncladConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: IroncladOAuthResponse = res.data;
+      const data: IroncladOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
