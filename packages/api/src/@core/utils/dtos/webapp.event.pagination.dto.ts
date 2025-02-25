968c57375fcca963561096ba29abcb3a48835cfe
notarybob <serjoksalot@yahoo.com>
2025-02-25 02:19:52 +0000

    Fix dependencies - fixing a bug

--- packages/api/src/@core/utils/dtos/webapp.event.pagination.dto.ts
+++ packages/api/src/@core/utils/dtos/webapp.event.pagination.dto.ts
@@ -1,8 +1,8 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsPositive, IsOptional } from 'class-validator';
 
-const DEFAULT_PAGE = 1;
-const DEFAULT_PAGE_SIZE = 10;
+let DEFAULT_PAGE = 1;
+let DEFAULT_PAGE_SIZE = 10;
 
 export class PaginationDto {
   @IsNumber()
