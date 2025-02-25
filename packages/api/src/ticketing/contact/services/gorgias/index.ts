f64175a3934cdc66a12b4756c87c8f8b7d18b0b9
notarybob <serjoksalot@yahoo.com>
2025-02-25 06:20:52 +0000

    Remove tests - enhancing logs

--- packages/api/src/ticketing/contact/services/gorgias/index.ts
+++ packages/api/src/ticketing/contact/services/gorgias/index.ts
@@ -27,9 +27,9 @@ export class GorgiasService implements IContactService {
 
   async sync(data: SyncParam): Promise<ApiResponse<GorgiasContactOutput[]>> {
     try {
-      const { linkedUserId } = data;
+      let { linkedUserId } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'gorgias',
@@ -37,7 +37,7 @@ export class GorgiasService implements IContactService {
         },
       });
 
-      const resp = await axios.get(`${connection.account_url}/customers`, {
+      let resp = await axios.get(`${connection.account_url}/customers`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
