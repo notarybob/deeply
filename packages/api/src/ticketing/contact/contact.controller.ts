4ebe528626d8cf8e5d790e08d9f114450a2ff95e
notarybob <serjoksalot@yahoo.com>
2025-02-26 13:28:48 +0000

    Improve auth - handling edge cases

--- packages/api/src/ticketing/contact/contact.controller.ts
+++ packages/api/src/ticketing/contact/contact.controller.ts
@@ -59,11 +59,11 @@ export class ContactController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.contactService.getContacts(
         connectionId,
         projectId,
@@ -110,7 +110,7 @@ export class ContactController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
