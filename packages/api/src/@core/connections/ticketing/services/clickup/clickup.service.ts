4093348f70e05b085f3491572bc56b0646eca0f2
notarybob <serjoksalot@yahoo.com>
2025-02-28 04:25:48 +0000

    Revert database - fixing a bug

--- packages/api/src/@core/connections/ticketing/services/clickup/clickup.service.ts
+++ packages/api/src/@core/connections/ticketing/services/clickup/clickup.service.ts
@@ -52,10 +52,10 @@ export class ClickupConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -87,8 +87,8 @@ export class ClickupConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'clickup',
@@ -97,18 +97,18 @@ export class ClickupConnectionService extends AbstractBaseConnectionService {
       });
 
       //reconstruct the redirect URI that was passed in the githubend it must be the same
-      //var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      //const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         code: code,
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://api.clickup.com/api/v2/oauth/token`,
         formData.toString(),
         {
@@ -117,13 +117,13 @@ export class ClickupConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: ClickupOAuthResponse = res.data;
+      const data: ClickupOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : clickup ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
