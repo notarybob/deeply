bb6a353d301cb8aaf0074de2e6764d09a4252613
notarybob <serjoksalot@yahoo.com>
2025-03-01 02:07:28 +0000

    Update tests - fixing typo

--- packages/api/src/filestorage/drive/drive.controller.ts
+++ packages/api/src/filestorage/drive/drive.controller.ts
@@ -57,11 +57,11 @@ export class DriveController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
 
       return this.driveService.getDrives(
         connectionId,
@@ -111,7 +111,7 @@ export class DriveController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
