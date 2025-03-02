0a45043eeeb839b46117ddfdaec677ce564e42e1
notarybob <serjoksalot@yahoo.com>
2025-03-02 22:04:05 +0000

    Change dependencies - refactoring code

--- packages/api/src/crm/note/note.controller.ts
+++ packages/api/src/crm/note/note.controller.ts
@@ -66,11 +66,11 @@ export class NoteController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.noteService.getNotes(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class NoteController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -160,7 +160,7 @@ export class NoteController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
