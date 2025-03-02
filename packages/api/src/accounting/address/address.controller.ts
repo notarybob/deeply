f739f4afe0dbcd2a93cdb9e6aa80ba6e3015edf2
notarybob <serjoksalot@yahoo.com>
2025-03-02 14:38:29 +0000

    Add README - fixing typo

--- packages/api/src/accounting/address/address.controller.ts
+++ packages/api/src/accounting/address/address.controller.ts
@@ -64,11 +64,11 @@ export class AddressController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.addressService.getAddresss(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class AddressController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
