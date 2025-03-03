0f5fd7c578d35caf0c66b1263b57c0432619eea2
notarybob <serjoksalot@yahoo.com>
2025-03-03 07:16:02 +0000

    Change configuration - handling edge cases

--- packages/api/src/marketingautomation/@lib/@unification/index.ts
+++ packages/api/src/marketingautomation/@lib/@unification/index.ts
@@ -29,7 +29,7 @@ export class MarketingAutomationUnificationService implements IUnification {
       remote_id: string;
     }[];
   }): Promise<MarketingAutomationObjectInput> {
-    let mapping = this.mappersRegistry.getService(
+    const mapping = this.mappersRegistry.getService(
       'marketingautomation',
       targetType_,
       providerName,
@@ -60,7 +60,7 @@ export class MarketingAutomationUnificationService implements IUnification {
       remote_id: string;
     }[];
   }): Promise<UnifyReturnType> {
-    let mapping = this.mappersRegistry.getService(
+    const mapping = this.mappersRegistry.getService(
       'marketingautomation',
       targetType_,
       providerName,
