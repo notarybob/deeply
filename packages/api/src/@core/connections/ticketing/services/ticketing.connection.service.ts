581d5180c42b8545104f1c93b50399437a101961
notarybob <serjoksalot@yahoo.com>
2025-03-04 08:20:55 +0000

    Revert UI - fixing a bug

--- packages/api/src/@core/connections/ticketing/services/ticketing.connection.service.ts
+++ packages/api/src/@core/connections/ticketing/services/ticketing.connection.service.ts
@@ -30,7 +30,7 @@ export class TicketingConnectionsService implements IConnectionCategory {
   //STEP 1:[FRONTEND STEP]
   //create a frontend SDK snippet in which an authorization embedded link is set up  so when users click
   // on it to grant access => they grant US the access and then when confirmed
-  /*const authUrl =
+  /*let authUrl =
   'https://app.hubspot.com/oauth/authorize' +
   `?client_id=${encodeURIComponent(CLIENT_ID)}` +
   `&scope=${encodeURIComponent(SCOPES)}` +
@@ -47,15 +47,15 @@ export class TicketingConnectionsService implements IConnectionCategory {
     type_strategy: 'oauth2' | 'apikey' | 'basic',
   ) {
     try {
-      const serviceName = providerName.toLowerCase();
+      let serviceName = providerName.toLowerCase();
 
-      const service = this.serviceRegistry.getService(serviceName);
+      let service = this.serviceRegistry.getService(serviceName);
 
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${providerName}`);
       }
-      const data: Connection = await service.handleCallback(callbackOpts);
-      const event = await this.prisma.events.create({
+      let data: Connection = await service.handleCallback(callbackOpts);
+      let event = await this.prisma.events.create({
         data: {
           id_connection: data.id_connection,
           id_project: data.id_project,
@@ -90,12 +90,12 @@ export class TicketingConnectionsService implements IConnectionCategory {
     account_url?: string,
   ) {
     try {
-      const serviceName = providerName.toLowerCase();
-      const service = this.serviceRegistry.getService(serviceName);
+      let serviceName = providerName.toLowerCase();
+      let service = this.serviceRegistry.getService(serviceName);
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${providerName}`);
       }
-      const refreshOpts: RefreshParams = {
+      let refreshOpts: RefreshParams = {
         connectionId: connectionId,
         refreshToken: refresh_token,
         account_url: account_url,
@@ -112,13 +112,13 @@ export class TicketingConnectionsService implements IConnectionCategory {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const connection = await this.prisma.connections.findUnique({
+      let connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
       });
-      const serviceName = connection.provider_slug.toLowerCase();
-      const service = this.serviceRegistry.getService(serviceName);
+      let serviceName = connection.provider_slug.toLowerCase();
+      let service = this.serviceRegistry.getService(serviceName);
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${serviceName}`);
       }
