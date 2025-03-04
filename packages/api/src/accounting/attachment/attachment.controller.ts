0f5b6e2a952ffb1a079bf21503185b83505331f3
notarybob <serjoksalot@yahoo.com>
2025-03-04 21:07:51 +0000

    Revert build - enhancing logs

--- packages/api/src/accounting/attachment/attachment.controller.ts
+++ packages/api/src/accounting/attachment/attachment.controller.ts
@@ -65,11 +65,11 @@ export class AttachmentController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.attachmentService.getAttachments(
         connectionId,
         projectId,
@@ -119,7 +119,7 @@ export class AttachmentController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -162,7 +162,7 @@ export class AttachmentController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
