136439ec599a9303406dd49ba500c1532b5c23c3
notarybob <serjoksalot@yahoo.com>
2025-03-08 02:00:29 +0000

    Document database - fixing typo

--- packages/api/src/crm/note/services/close/index.ts
+++ packages/api/src/crm/note/services/close/index.ts
@@ -29,14 +29,14 @@ export class CloseService implements INoteService {
     linkedUserId: string,
   ): Promise<ApiResponse<CloseNoteOutput>> {
     try {
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'close',
           vertical: 'crm',
         },
       });
-      const resp = await axios.post(
+      let resp = await axios.post(
         `${connection.account_url}/v1/activity/note`,
         JSON.stringify(noteData),
         {
@@ -60,9 +60,9 @@ export class CloseService implements INoteService {
 
   async sync(data: SyncParam): Promise<ApiResponse<CloseNoteOutput[]>> {
     try {
-      const { linkedUserId } = data;
+      let { linkedUserId } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'close',
@@ -70,9 +70,9 @@ export class CloseService implements INoteService {
         },
       });
 
-      const baseURL = `${connection.account_url}/v1/activity/note`;
+      let baseURL = `${connection.account_url}/v1/activity/note`;
 
-      const resp = await axios.get(baseURL, {
+      let resp = await axios.get(baseURL, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
