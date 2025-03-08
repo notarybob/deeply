6e2d86c1ea7c6898d099e0655bca057c7fb2ed26
notarybob <serjoksalot@yahoo.com>
2025-03-08 19:04:17 +0000

    Update build - removing deprecated function

--- packages/api/src/filestorage/file/services/onedrive/mappers.ts
+++ packages/api/src/filestorage/file/services/onedrive/mappers.ts
@@ -81,9 +81,9 @@ export class OnedriveFileMapper implements IFileMapper {
       remote_id: string;
     }[],
   ): Promise<UnifiedFilestorageFileOutput> {
-    let field_mappings: { [key: string]: any } = {};
+    const field_mappings: { [key: string]: any } = {};
     if (customFieldMappings) {
-      for (let mapping of customFieldMappings) {
+      for (const mapping of customFieldMappings) {
         field_mappings[mapping.slug] = file[mapping.remote_id];
       }
     }
