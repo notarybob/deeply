0ae3d52e45e8ff7c3fe799e8ed06e369f28761bf
notarybob <serjoksalot@yahoo.com>
2025-03-08 04:59:32 +0000

    Fix database - handling edge cases

--- packages/api/src/filestorage/drive/services/registry.service.ts
+++ packages/api/src/filestorage/drive/services/registry.service.ts
@@ -14,7 +14,7 @@ export class ServiceRegistry {
   }
 
   getService(integrationId: string): IDriveService {
-    const service = this.serviceMap.get(integrationId);
+    let service = this.serviceMap.get(integrationId);
     if (!service) {
       //throw new ReferenceError();
       return null;
