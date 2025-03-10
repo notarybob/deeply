44b3c07825b893465e99d3fb7f755256bcc377b1
notarybob <serjoksalot@yahoo.com>
2025-03-10 16:58:08 +0000

    Update API - updating docs

--- packages/api/src/ticketing/team/services/registry.service.ts
+++ packages/api/src/ticketing/team/services/registry.service.ts
@@ -14,7 +14,7 @@ export class ServiceRegistry {
   }
 
   getService(integrationId: string): ITeamService {
-    const service = this.serviceMap.get(integrationId);
+    let service = this.serviceMap.get(integrationId);
     if (!service) {
       return null;
     }
