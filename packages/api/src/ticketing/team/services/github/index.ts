375724b1128bb98a0a812261c9121031cf406cf1
notarybob <serjoksalot@yahoo.com>
2025-03-12 15:54:49 +0000

    Refactor dependencies - fixing typo

--- packages/api/src/ticketing/team/services/github/index.ts
+++ packages/api/src/ticketing/team/services/github/index.ts
@@ -27,9 +27,9 @@ export class GithubService implements ITeamService {
 
     async sync(data: SyncParam): Promise<ApiResponse<GithubTeamOutput[]>> {
         try {
-            const { linkedUserId } = data;
+            let { linkedUserId } = data;
 
-            const connection = await this.prisma.connections.findFirst({
+            let connection = await this.prisma.connections.findFirst({
                 where: {
                     id_linked_user: linkedUserId,
                     provider_slug: 'github',
@@ -37,7 +37,7 @@ export class GithubService implements ITeamService {
                 },
             });
 
-            const resp = await axios.get(`${connection.account_url}/user/teams`, {
+            let resp = await axios.get(`${connection.account_url}/user/teams`, {
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${this.cryptoService.decrypt(
