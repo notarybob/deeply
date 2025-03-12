b575f338b013ccbfede2a16c8fe4f743677255ac
notarybob <serjoksalot@yahoo.com>
2025-03-12 04:03:11 +0000

    Fix API - fixing a bug

--- packages/api/src/@core/connections/filestorage/services/box/box.service.ts
+++ packages/api/src/@core/connections/filestorage/services/box/box.service.ts
@@ -56,10 +56,10 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -91,8 +91,8 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'box',
@@ -100,18 +100,18 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         code: code,
         grant_type: 'authorization_code',
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://api.box.com/oauth2/token`,
         formData.toString(),
         {
@@ -120,10 +120,10 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: BoxOAuthResponse = res.data;
+      const data: BoxOAuthResponse = res.data;
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -180,21 +180,21 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
   }
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
+      const { connectionId, refreshToken, projectId } = opts;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         `https://api.box.com/oauth2/token`,
         formData.toString(),
         {
@@ -203,7 +203,7 @@ export class BoxConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: BoxOAuthResponse = res.data;
+      const data: BoxOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
