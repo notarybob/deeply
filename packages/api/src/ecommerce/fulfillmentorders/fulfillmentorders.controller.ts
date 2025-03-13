5c21c5f2fb1f5f70b23df0866b125669f3538afb
notarybob <serjoksalot@yahoo.com>
2025-03-13 05:25:08 +0000

    Remove dependencies - updating docs

--- packages/api/src/ecommerce/fulfillmentorders/fulfillmentorders.controller.ts
+++ packages/api/src/ecommerce/fulfillmentorders/fulfillmentorders.controller.ts
@@ -50,11 +50,11 @@ export class FulfillmentOrdersController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.fulfillmentordersService.getFulfillmentOrderss(
         connectionId,
         projectId,
@@ -102,7 +102,7 @@ export class FulfillmentOrdersController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
