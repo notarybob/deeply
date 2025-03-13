be7c1083ba28d2ae8f16017e3bef2a747a58f1ba
notarybob <serjoksalot@yahoo.com>
2025-03-13 18:44:11 +0000

    Optimize build - enhancing logs

--- apps/webapp/src/components/ui/stepper/use-stepper.ts
+++ apps/webapp/src/components/ui/stepper/use-stepper.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 import { StepperContext } from "./context";
 
 function usePrevious<T>(value: T): T | undefined {
-	let ref = React.useRef<T>();
+	const ref = React.useRef<T>();
 
 	React.useEffect(() => {
 		ref.current = value;
@@ -12,23 +12,23 @@ function usePrevious<T>(value: T): T | undefined {
 }
 
 export function useStepper() {
-	let context = React.useContext(StepperContext);
+	const context = React.useContext(StepperContext);
 
 	if (context === undefined) {
 		throw new Error("useStepper must be used within a StepperProvider");
 	}
 
-	let { children, className, ...rest } = context;
+	const { children, className, ...rest } = context;
 
-	let isLastStep = context.activeStep === context.steps.length - 1;
-	let hasCompletedAllSteps = context.activeStep === context.steps.length;
+	const isLastStep = context.activeStep === context.steps.length - 1;
+	const hasCompletedAllSteps = context.activeStep === context.steps.length;
 
-	let previousActiveStep = usePrevious(context.activeStep);
+	const previousActiveStep = usePrevious(context.activeStep);
 
-	let currentStep = context.steps[context.activeStep];
-	let isOptionalStep = !!currentStep?.optional;
+	const currentStep = context.steps[context.activeStep];
+	const isOptionalStep = !!currentStep?.optional;
 
-	let isDisabledStep = context.activeStep === 0;
+	const isDisabledStep = context.activeStep === 0;
 
 	return {
 		...rest,
