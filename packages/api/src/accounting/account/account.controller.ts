d690a0e28090c0d741d56586c0be2700ad2d1295
notarybob <serjoksalot@yahoo.com>
2025-03-14 05:19:13 +0000

    Update dependencies - improving performance

--- packages/api/src/accounting/account/account.controller.ts
+++ packages/api/src/accounting/account/account.controller.ts
@@ -67,11 +67,11 @@ export class AccountController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.accountService.getAccounts(
         connectionId,
         projectId,
@@ -120,7 +120,7 @@ export class AccountController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -163,7 +163,7 @@ export class AccountController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
