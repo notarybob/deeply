a0ff2c3f4694643d33a9206da73a6b313c5af012
notarybob <serjoksalot@yahoo.com>
2025-03-16 23:39:33 +0000

    Refactor dependencies - fixing a bug

--- packages/api/src/accounting/creditnote/creditnote.controller.ts
+++ packages/api/src/accounting/creditnote/creditnote.controller.ts
@@ -65,11 +65,11 @@ export class CreditNoteController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.creditnoteService.getCreditNotes(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class CreditNoteController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
