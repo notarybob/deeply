4c3dcf32f8cf59429cd5bb16560f6b0afcdd7b3d
notarybob <serjoksalot@yahoo.com>
2025-03-17 04:15:43 +0000

    Update tests - fixing typo

--- packages/api/src/@core/connections/ticketing/services/aha/aha.service.ts
+++ packages/api/src/@core/connections/ticketing/services/aha/aha.service.ts
@@ -54,10 +54,10 @@ export class AhaConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -93,8 +93,8 @@ export class AhaConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'aha',
@@ -102,25 +102,25 @@ export class AhaConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      var REDIRECT_URI = `${
+      const REDIRECT_URI = `${
         this.env.getDistributionMode() == 'selfhost'
           ? this.env.getTunnelIngress()
           : this.env.getPanoraBaseUrl()
       }/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://${CREDENTIALS.SUBDOMAIN}.aha.io/oauth/token`,
         formData.toString(),
         {
@@ -129,15 +129,15 @@ export class AhaConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      var data: AhaOAuthResponse = res.data;
+      const data: AhaOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : aha ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
-      var BASE_API_URL = (
+      const BASE_API_URL = (
         CONNECTORS_METADATA['ticketing']['aha'].urls.apiUrl as DynamicApiUrl
       )(CREDENTIALS.SUBDOMAIN);
 
