88e11d799eb4e73c70cefeabf80386744fb132c8
notarybob <serjoksalot@yahoo.com>
2025-03-18 15:13:55 +0000

    Change database - fixing a bug

--- packages/api/src/ticketing/collection/services/gitlab/mappers.ts
+++ packages/api/src/ticketing/collection/services/gitlab/mappers.ts
@@ -25,7 +25,7 @@ export class GitlabCollectionMapper implements ICollectionMapper {
       remote_id: string;
     }[],
   ): GitlabCollectionInput {
-    const result: GitlabCollectionInput = {
+    let result: GitlabCollectionInput = {
       name: source.name,
       description: source.description ? source.description : null,
       path: source.name,
@@ -43,7 +43,7 @@ export class GitlabCollectionMapper implements ICollectionMapper {
     }[],
   ): UnifiedTicketingCollectionOutput | UnifiedTicketingCollectionOutput[] {
     // If the source is not an array, convert it to an array for mapping
-    const sourcesArray = Array.isArray(source) ? source : [source];
+    let sourcesArray = Array.isArray(source) ? source : [source];
 
     return sourcesArray.map((collection) =>
       this.mapSingleCollectionToUnified(
@@ -62,7 +62,7 @@ export class GitlabCollectionMapper implements ICollectionMapper {
       remote_id: string;
     }[],
   ): UnifiedTicketingCollectionOutput {
-    const unifiedCollection: UnifiedTicketingCollectionOutput = {
+    let unifiedCollection: UnifiedTicketingCollectionOutput = {
       remote_id: String(collection.id),
       remote_data: collection,
       name: collection.name,
