147602a49201c0a7b15bd883959153bf76c20f08
notarybob <serjoksalot@yahoo.com>
2025-03-18 21:52:20 +0000

    Optimize UI - improving performance

--- packages/api/src/@core/connections/crm/services/microsoftdynamicssales/microsoftdynamicssales.service.ts
+++ packages/api/src/@core/connections/crm/services/microsoftdynamicssales/microsoftdynamicssales.service.ts
@@ -56,10 +56,10 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -91,8 +91,8 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code, organization_name } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code, organization_name } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'microsoftdynamicssales',
@@ -100,14 +100,14 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
         },
       });
 
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
 
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         redirect_uri: REDIRECT_URI,
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
@@ -115,7 +115,7 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
         scope: `https://${organization_name}/.default offline_access`,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         `https://login.microsoftonline.com/common/oauth2/v2.0/token`,
         formData.toString(),
         {
@@ -124,14 +124,14 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
           },
         },
       );
-      var data: MicrosoftDynamicsSalesOAuthResponse = res.data;
+      const data: MicrosoftDynamicsSalesOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : microsoftdynamicssales crm ' +
           JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -186,20 +186,20 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
   }
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var conn = await this.prisma.connections.findUnique({
+      const conn = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
       });
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         scope: `${conn.account_url}/.default offline_access`,
         client_id: CREDENTIALS.CLIENT_ID,
@@ -208,7 +208,7 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
         redirect_uri: REDIRECT_URI,
       });
 
-      var res = await axios.post(
+      const res = await axios.post(
         `https://login.microsoftonline.com/common/oauth2/v2.0/token`,
         formData.toString(),
         {
@@ -217,7 +217,7 @@ export class MicrosoftDynamicsSalesConnectionService extends AbstractBaseConnect
           },
         },
       );
-      var data: MicrosoftDynamicsSalesOAuthResponse = res.data;
+      const data: MicrosoftDynamicsSalesOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
