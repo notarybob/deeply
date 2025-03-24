9e6d5166b737f6838eecbac42d09ac4fb6fb3b17
notarybob <serjoksalot@yahoo.com>
2025-03-24 09:46:10 +0000

    Document tests - improving performance

--- packages/api/src/marketingautomation/message/message.controller.ts
+++ packages/api/src/marketingautomation/message/message.controller.ts
@@ -63,11 +63,11 @@ export class MessageController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.messageService.getMessages(
         connectionId,
         projectId,
@@ -117,7 +117,7 @@ export class MessageController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
