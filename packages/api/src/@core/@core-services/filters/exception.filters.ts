d64f849e5053f89633174e977ede40b0b7b513f3
notarybob <serjoksalot@yahoo.com>
2025-03-27 22:05:19 +0000

    Change database - improving performance

--- packages/api/src/@core/@core-services/filters/exception.filters.ts
+++ packages/api/src/@core/@core-services/filters/exception.filters.ts
@@ -11,10 +11,10 @@ export class HttpExceptionFilter {
   ) {}
 
   catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    let ctx = host.switchToHttp();
+    let response = ctx.getResponse<Response>();
+    let request = ctx.getRequest<Request>();
+    let status = exception.getStatus();
 
     this.logger.error(
       `HTTP Exception: ${exception.getResponse()}`,
