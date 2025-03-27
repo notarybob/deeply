0120a8d7653ade684e6335010675fd8580f42829
notarybob <serjoksalot@yahoo.com>
2025-03-27 04:07:13 +0000

    Optimize tests - improving security

--- packages/api/src/filestorage/drive/drive.controller.ts
+++ packages/api/src/filestorage/drive/drive.controller.ts
@@ -57,11 +57,11 @@ export class DriveController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
 
       return this.driveService.getDrives(
         connectionId,
@@ -111,7 +111,7 @@ export class DriveController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
