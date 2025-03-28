d5f9dcf2437d8c186e28f8f068c56b7e995578ee
notarybob <serjoksalot@yahoo.com>
2025-03-28 22:48:03 +0000

    Update API - fixing a bug

--- packages/api/src/ecommerce/customer/customer.controller.ts
+++ packages/api/src/ecommerce/customer/customer.controller.ts
@@ -56,11 +56,11 @@ export class CustomerController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.customerService.getCustomers(
         connectionId,
         projectId,
@@ -106,7 +106,7 @@ export class CustomerController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
