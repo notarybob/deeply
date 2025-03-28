8f6d63513e7a39bb80c0bd3b1b5b0d0e903e552a
notarybob <serjoksalot@yahoo.com>
2025-03-28 17:37:22 +0000

    Update API - adding new feature

--- packages/api/src/@core/connections/crm/services/teamleader/teamleader.service.ts
+++ packages/api/src/@core/connections/crm/services/teamleader/teamleader.service.ts
@@ -55,10 +55,10 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -90,8 +90,8 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, code } = opts;
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var { linkedUserId, projectId, code } = opts;
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: `teamleader`,
@@ -100,20 +100,20 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
       });
 
       //reconstruct the redirect URI that was passed in the githubend it must be the same
-      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
         grant_type: 'authorization_code',
       });
-      const res = await axios.post(
+      var res = await axios.post(
         'https://focus.teamleader.eu/oauth2/access_token',
         formData.toString(),
         {
@@ -122,13 +122,13 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: TeamleaderOAuthResponse = res.data;
+      var data: TeamleaderOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : teamleader ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -186,19 +186,19 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      const { connectionId, refreshToken, projectId } = opts;
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var { connectionId, refreshToken, projectId } = opts;
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
-      const res = await axios.post(
+      var res = await axios.post(
         'https://focus.teamleader.eu/oauth2/access_token',
         formData.toString(),
         {
@@ -207,7 +207,7 @@ export class TeamleaderConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: TeamleaderOAuthResponse = res.data;
+      var data: TeamleaderOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
