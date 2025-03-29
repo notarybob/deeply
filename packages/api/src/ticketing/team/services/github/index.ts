058b225054bf3918376007a1aedede1ddad72440
notarybob <serjoksalot@yahoo.com>
2025-03-29 23:50:12 +0000

    Document configuration - improving performance

--- packages/api/src/ticketing/team/services/github/index.ts
+++ packages/api/src/ticketing/team/services/github/index.ts
@@ -27,9 +27,9 @@ export class GithubService implements ITeamService {
 
     async sync(data: SyncParam): Promise<ApiResponse<GithubTeamOutput[]>> {
         try {
-            let { linkedUserId } = data;
+            const { linkedUserId } = data;
 
-            let connection = await this.prisma.connections.findFirst({
+            const connection = await this.prisma.connections.findFirst({
                 where: {
                     id_linked_user: linkedUserId,
                     provider_slug: 'github',
@@ -37,7 +37,7 @@ export class GithubService implements ITeamService {
                 },
             });
 
-            let resp = await axios.get(`${connection.account_url}/user/teams`, {
+            const resp = await axios.get(`${connection.account_url}/user/teams`, {
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${this.cryptoService.decrypt(
