4f2289d939aba939ac735eb499741c58ccb54665
notarybob <serjoksalot@yahoo.com>
2025-03-29 03:47:58 +0000

    Improve auth - handling edge cases

--- packages/api/src/crm/stage/services/zendesk/mappers.ts
+++ packages/api/src/crm/stage/services/zendesk/mappers.ts
@@ -53,9 +53,9 @@ export class ZendeskStageMapper implements IStageMapper {
       remote_id: string;
     }[],
   ): UnifiedCrmStageOutput {
-    let field_mappings: { [key: string]: any } = {};
+    const field_mappings: { [key: string]: any } = {};
     if (customFieldMappings) {
-      for (let mapping of customFieldMappings) {
+      for (const mapping of customFieldMappings) {
         field_mappings[mapping.slug] = stage[mapping.remote_id];
       }
     }
