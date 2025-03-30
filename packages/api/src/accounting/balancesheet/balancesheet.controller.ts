68d9d623a6edaa5aaf734a6403d13f2c6cc2b16a
notarybob <serjoksalot@yahoo.com>
2025-03-30 14:19:19 +0000

    Refactor dependencies - improving performance

--- packages/api/src/accounting/balancesheet/balancesheet.controller.ts
+++ packages/api/src/accounting/balancesheet/balancesheet.controller.ts
@@ -65,11 +65,11 @@ export class BalanceSheetController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.balancesheetService.getBalanceSheets(
         connectionId,
         projectId,
@@ -119,7 +119,7 @@ export class BalanceSheetController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
