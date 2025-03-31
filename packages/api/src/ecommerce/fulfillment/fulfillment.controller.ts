502de25bcc67121f2c1c165bd23bc4607493dba7
notarybob <serjoksalot@yahoo.com>
2025-03-31 15:03:16 +0000

    Refactor UI - adding new feature

--- packages/api/src/ecommerce/fulfillment/fulfillment.controller.ts
+++ packages/api/src/ecommerce/fulfillment/fulfillment.controller.ts
@@ -56,11 +56,11 @@ export class FulfillmentController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.fulfillmentService.getFulfillments(
         connectionId,
         projectId,
@@ -106,7 +106,7 @@ export class FulfillmentController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
