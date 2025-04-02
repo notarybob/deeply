a7bb7aae5c4ea03c8d9445c918801f56116f775e
notarybob <serjoksalot@yahoo.com>
2025-04-02 21:25:25 +0000

    Fix styles - fixing typo

--- packages/api/src/@core/connections/ecommerce/services/ecommerce.connection.service.ts
+++ packages/api/src/@core/connections/ecommerce/services/ecommerce.connection.service.ts
@@ -29,7 +29,7 @@ export class EcommerceConnectionsService implements IConnectionCategory {
   //STEP 1:[FRONTEND STEP]
   //create a frontend SDK snippet in which an authorization embedded link is set up  so when users click
   // on it to grant access => they grant US the access and then when confirmed
-  /*let authUrl =
+  /*const authUrl =
   'https://app.hubspot.com/oauth/authorize' +
   `?client_id=${encodeURIComponent(CLIENT_ID)}` +
   `&scope=${encodeURIComponent(SCOPES)}` +
@@ -46,15 +46,15 @@ export class EcommerceConnectionsService implements IConnectionCategory {
     type_strategy: 'oauth2' | 'apikey' | 'basic',
   ) {
     try {
-      let serviceName = providerName.toLowerCase();
+      const serviceName = providerName.toLowerCase();
 
-      let service = this.serviceRegistry.getService(serviceName);
+      const service = this.serviceRegistry.getService(serviceName);
 
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${providerName}`);
       }
-      let data: Connection = await service.handleCallback(callbackOpts);
-      let event = await this.prisma.events.create({
+      const data: Connection = await service.handleCallback(callbackOpts);
+      const event = await this.prisma.events.create({
         data: {
           id_connection: data.id_connection,
           id_project: data.id_project,
@@ -89,12 +89,12 @@ export class EcommerceConnectionsService implements IConnectionCategory {
     account_url?: string,
   ) {
     try {
-      let serviceName = providerName.toLowerCase();
-      let service = this.serviceRegistry.getService(serviceName);
+      const serviceName = providerName.toLowerCase();
+      const service = this.serviceRegistry.getService(serviceName);
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${providerName}`);
       }
-      let refreshOpts: RefreshParams = {
+      const refreshOpts: RefreshParams = {
         connectionId: connectionId,
         refreshToken: refresh_token,
         account_url: account_url,
@@ -111,13 +111,13 @@ export class EcommerceConnectionsService implements IConnectionCategory {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      let connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
       });
-      let serviceName = connection.provider_slug.toLowerCase();
-      let service = this.serviceRegistry.getService(serviceName);
+      const serviceName = connection.provider_slug.toLowerCase();
+      const service = this.serviceRegistry.getService(serviceName);
       if (!service) {
         throw new ReferenceError(`Unknown provider, found ${serviceName}`);
       }
