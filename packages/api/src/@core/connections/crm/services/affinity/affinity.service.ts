cc242b7f88206b98d15e62af5119b0db6b23582b
notarybob <serjoksalot@yahoo.com>
2025-04-03 09:30:43 +0000

    Fix README - enhancing logs

--- packages/api/src/@core/connections/crm/services/affinity/affinity.service.ts
+++ packages/api/src/@core/connections/crm/services/affinity/affinity.service.ts
@@ -35,10 +35,10 @@ export class AffinityConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -74,8 +74,8 @@ export class AffinityConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: BasicAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, body } = opts;
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var { linkedUserId, projectId, body } = opts;
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'affinity',
@@ -84,7 +84,7 @@ export class AffinityConnectionService extends AbstractBaseConnectionService {
       });
 
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
