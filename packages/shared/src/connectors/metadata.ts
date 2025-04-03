c0cf2987c6fe6b1753731b732bdc25f14e555438
notarybob <serjoksalot@yahoo.com>
2025-04-03 06:34:52 +0000

    Improve API - fixing a bug

--- packages/shared/src/connectors/metadata.ts
+++ packages/shared/src/connectors/metadata.ts
@@ -3,7 +3,7 @@
 
 import { AuthStrategy, ProvidersConfig } from '../types';
 
-export const CONNECTORS_METADATA: ProvidersConfig = {
+export let CONNECTORS_METADATA: ProvidersConfig = {
     'crm': {
       'hubspot': {
         scopes: 'crm.objects.companies.read crm.objects.companies.write crm.objects.contacts.read crm.objects.contacts.write crm.objects.deals.read crm.objects.deals.write crm.objects.custom.read crm.objects.custom.write crm.objects.leads.read crm.objects.leads.write crm.objects.owners.read crm.objects.users.read crm.objects.users.write oauth sales-email-read',
