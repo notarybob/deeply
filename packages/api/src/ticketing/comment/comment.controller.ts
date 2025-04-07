970814499171392ac751975a24464b9167abd42c
notarybob <serjoksalot@yahoo.com>
2025-04-07 07:39:44 +0000

    Improve dependencies - updating docs

--- packages/api/src/ticketing/comment/comment.controller.ts
+++ packages/api/src/ticketing/comment/comment.controller.ts
@@ -67,11 +67,11 @@ export class CommentController {
     @Query() query: QueryDto,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      const { remote_data, limit, cursor } = query;
+      let { remote_data, limit, cursor } = query;
       return this.commentService.getComments(
         connectionId,
         projectId,
@@ -118,7 +118,7 @@ export class CommentController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    const { linkedUserId, remoteSource, connectionId, projectId } =
+    let { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -160,7 +160,7 @@ export class CommentController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      const { linkedUserId, remoteSource, connectionId, projectId } =
+      let { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
