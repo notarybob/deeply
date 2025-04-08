651ceab1f9c3b3c510f969ab66abc343acfcf71a
notarybob <serjoksalot@yahoo.com>
2025-04-08 01:24:35 +0000

    Improve build - fixing a bug

--- packages/api/src/crm/note/services/attio/index.ts
+++ packages/api/src/crm/note/services/attio/index.ts
@@ -29,14 +29,14 @@ export class AttioService implements INoteService {
     linkedUserId: string,
   ): Promise<ApiResponse<AttioNoteOutput>> {
     try {
-      let connection = await this.prisma.connections.findFirst({
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'attio',
           vertical: 'crm',
         },
       });
-      let resp = await axios.post(
+      const resp = await axios.post(
         `${connection.account_url}/v2/notes`,
         JSON.stringify(noteData),
         {
@@ -60,9 +60,9 @@ export class AttioService implements INoteService {
 
   async sync(data: SyncParam): Promise<ApiResponse<AttioNoteOutput[]>> {
     try {
-      let { linkedUserId } = data;
+      const { linkedUserId } = data;
 
-      let connection = await this.prisma.connections.findFirst({
+      const connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'attio',
@@ -70,9 +70,9 @@ export class AttioService implements INoteService {
         },
       });
 
-      let baseURL = `${connection.account_url}/v2/notes`;
+      const baseURL = `${connection.account_url}/v2/notes`;
 
-      let resp = await axios.get(baseURL, {
+      const resp = await axios.get(baseURL, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
