785ed7898cfbb092808fcf1d91821e870f7b5dce
notarybob <serjoksalot@yahoo.com>
2025-04-08 16:39:32 +0000

    Refactor build - improving performance

--- packages/api/src/ticketing/user/services/front/index.ts
+++ packages/api/src/ticketing/user/services/front/index.ts
@@ -27,9 +27,9 @@ export class FrontService implements IUserService {
 
   async sync(data: SyncParam): Promise<ApiResponse<FrontUserOutput[]>> {
     try {
-      const { linkedUserId } = data;
+      let { linkedUserId } = data;
 
-      const connection = await this.prisma.connections.findFirst({
+      let connection = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'front',
@@ -37,7 +37,7 @@ export class FrontService implements IUserService {
         },
       });
 
-      const resp = await axios.get(`${connection.account_url}/teammates`, {
+      let resp = await axios.get(`${connection.account_url}/teammates`, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${this.cryptoService.decrypt(
