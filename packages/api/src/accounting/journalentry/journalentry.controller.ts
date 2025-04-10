e91b05f44c21cf5fa9248ce070053a7a242708b1
notarybob <serjoksalot@yahoo.com>
2025-04-10 15:50:33 +0000

    Update auth - fixing a bug

--- packages/api/src/accounting/journalentry/journalentry.controller.ts
+++ packages/api/src/accounting/journalentry/journalentry.controller.ts
@@ -66,11 +66,11 @@ export class JournalEntryController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.journalentryService.getJournalEntrys(
         connectionId,
         projectId,
@@ -120,7 +120,7 @@ export class JournalEntryController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
@@ -163,7 +163,7 @@ export class JournalEntryController {
     @Query('remote_data') remote_data?: boolean,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
