3eb00748112c0b3a0d1ca013a3fee2ed1930f1c1
notarybob <serjoksalot@yahoo.com>
2025-04-13 03:35:25 +0000

    Document README - adding new feature

--- packages/api/src/@core/connections/marketingautomation/services/getresponse/getresponse.service.ts
+++ packages/api/src/@core/connections/marketingautomation/services/getresponse/getresponse.service.ts
@@ -60,10 +60,10 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      var { headers } = input;
-      var config = await this.constructPassthrough(input, connectionId);
+      const { headers } = input;
+      const config = await this.constructPassthrough(input, connectionId);
 
-      var connection = await this.prisma.connections.findUnique({
+      const connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -95,8 +95,8 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      var { linkedUserId, projectId, code } = opts;
-      var isNotUnique = await this.prisma.connections.findFirst({
+      const { linkedUserId, projectId, code } = opts;
+      const isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'getresponse',
@@ -105,17 +105,17 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
       });
 
       //reconstruct the redirect URI that was passed in the githubend it must be the same
-      var REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const REDIRECT_URI = `${this.env.getPanoraBaseUrl()}/connections/oauth/callback`;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         code: code,
         grant_type: 'authorization_code',
       });
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.getresponse.com/v3/token',
         formData.toString(),
         {
@@ -127,13 +127,13 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
           },
         },
       );
-      var data: GetresponseOAuthResponse = res.data;
+      const data: GetresponseOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : getresponse ticketing ' + JSON.stringify(data),
       );
 
       let db_res;
-      var connection_token = uuidv4();
+      const connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -193,16 +193,16 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      var { connectionId, refreshToken, projectId } = opts;
-      var CREDENTIALS = (await this.cService.getCredentials(
+      const { connectionId, refreshToken, projectId } = opts;
+      const CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
-      var formData = new URLSearchParams({
+      const formData = new URLSearchParams({
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
-      var res = await axios.post(
+      const res = await axios.post(
         'https://api.getresponse.com/v3/token',
         formData.toString(),
         {
@@ -214,7 +214,7 @@ export class GetresponseConnectionService extends AbstractBaseConnectionService 
           },
         },
       );
-      var data: GetresponseOAuthResponse = res.data;
+      const data: GetresponseOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
