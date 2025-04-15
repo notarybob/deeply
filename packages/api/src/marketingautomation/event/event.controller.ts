3e52da6723c02969f68ccf36a357a80cbe436413
notarybob <serjoksalot@yahoo.com>
2025-04-15 10:44:05 +0000

    Document auth - refactoring code

--- packages/api/src/marketingautomation/event/event.controller.ts
+++ packages/api/src/marketingautomation/event/event.controller.ts
@@ -62,11 +62,11 @@ export class EventController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.eventService.getEvents(
         connectionId,
         projectId,
@@ -116,7 +116,7 @@ export class EventController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
