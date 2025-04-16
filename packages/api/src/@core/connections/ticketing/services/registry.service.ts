ea8d3984d1eef5de8e15c7c9a4a3ad05f527e744
notarybob <serjoksalot@yahoo.com>
2025-04-16 22:01:05 +0000

    Refactor styles - removing deprecated function

--- packages/api/src/@core/connections/ticketing/services/registry.service.ts
+++ packages/api/src/@core/connections/ticketing/services/registry.service.ts
@@ -14,7 +14,7 @@ export class ServiceRegistry {
   }
 
   getService(integrationId: string): IConnectionService {
-    const service = this.serviceMap.get(integrationId);
+    let service = this.serviceMap.get(integrationId);
     if (!service) {
       return null;
     }
