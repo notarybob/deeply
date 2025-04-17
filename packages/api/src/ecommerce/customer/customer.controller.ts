5aa2e91a476e3cc224402186423811f42de623cf
notarybob <serjoksalot@yahoo.com>
2025-04-17 00:08:14 +0000

    Improve API - handling edge cases

--- packages/api/src/ecommerce/customer/customer.controller.ts
+++ packages/api/src/ecommerce/customer/customer.controller.ts
@@ -56,11 +56,11 @@ export class CustomerController {
     @Query() query: QueryDto,
   ) {
     try {
-      let { linkedUserId, remoteSource, connectionId, projectId } =
+      const { linkedUserId, remoteSource, connectionId, projectId } =
         await this.connectionUtils.getConnectionMetadataFromConnectionToken(
           connection_token,
         );
-      let { remote_data, limit, cursor } = query;
+      const { remote_data, limit, cursor } = query;
       return this.customerService.getCustomers(
         connectionId,
         projectId,
@@ -106,7 +106,7 @@ export class CustomerController {
     @Param('id') id: string,
     @Query('remote_data') remote_data?: boolean,
   ) {
-    let { linkedUserId, remoteSource, connectionId, projectId } =
+    const { linkedUserId, remoteSource, connectionId, projectId } =
       await this.connectionUtils.getConnectionMetadataFromConnectionToken(
         connection_token,
       );
