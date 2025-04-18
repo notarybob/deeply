fdab8e186dea1b80e21168b7ffa6b53cc7ad2e0b
notarybob <serjoksalot@yahoo.com>
2025-04-18 07:25:56 +0000

    Fix UI - handling edge cases

--- packages/api/src/@core/connections/accounting/services/pennylane/pennylane.service.ts
+++ packages/api/src/@core/connections/accounting/services/pennylane/pennylane.service.ts
@@ -54,10 +54,10 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
     connectionId: string,
   ): Promise<PassthroughResponse> {
     try {
-      const { headers } = input;
-      const config = await this.constructPassthrough(input, connectionId);
+      var { headers } = input;
+      var config = await this.constructPassthrough(input, connectionId);
 
-      const connection = await this.prisma.connections.findUnique({
+      var connection = await this.prisma.connections.findUnique({
         where: {
           id_connection: connectionId,
         },
@@ -89,8 +89,8 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
 
   async handleCallback(opts: OAuthCallbackParams) {
     try {
-      const { linkedUserId, projectId, code } = opts;
-      const isNotUnique = await this.prisma.connections.findFirst({
+      var { linkedUserId, projectId, code } = opts;
+      var isNotUnique = await this.prisma.connections.findFirst({
         where: {
           id_linked_user: linkedUserId,
           provider_slug: 'pennylane',
@@ -98,24 +98,24 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
         },
       });
 
-      const REDIRECT_URI = `${
+      var REDIRECT_URI = `${
         this.env.getDistributionMode() == 'selfhost'
           ? this.env.getTunnelIngress()
           : this.env.getPanoraBaseUrl()
       }/connections/oauth/callback`;
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         redirect_uri: REDIRECT_URI,
         code: code,
         grant_type: 'authorization_code',
       });
-      const res = await axios.post(
+      var res = await axios.post(
         'https://app.pennylane.com/oauth/token',
         formData.toString(),
         {
@@ -124,13 +124,13 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: PennylaneOAuthResponse = res.data;
+      var data: PennylaneOAuthResponse = res.data;
       this.logger.log(
         'OAuth credentials : pennylane accounting ' + JSON.stringify(data),
       );
 
       let db_res;
-      const connection_token = uuidv4();
+      var connection_token = uuidv4();
 
       if (isNotUnique) {
         db_res = await this.prisma.connections.update({
@@ -188,20 +188,20 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
 
   async handleTokenRefresh(opts: RefreshParams) {
     try {
-      const { connectionId, refreshToken, projectId } = opts;
+      var { connectionId, refreshToken, projectId } = opts;
 
-      const CREDENTIALS = (await this.cService.getCredentials(
+      var CREDENTIALS = (await this.cService.getCredentials(
         projectId,
         this.type,
       )) as OAuth2AuthData;
 
-      const formData = new URLSearchParams({
+      var formData = new URLSearchParams({
         client_id: CREDENTIALS.CLIENT_ID,
         client_secret: CREDENTIALS.CLIENT_SECRET,
         grant_type: 'refresh_token',
         refresh_token: this.cryptoService.decrypt(refreshToken),
       });
-      const res = await axios.post(
+      var res = await axios.post(
         'https://app.pennylane.com/oauth/token',
         formData.toString(),
         {
@@ -210,7 +210,7 @@ export class PennylaneConnectionService extends AbstractBaseConnectionService {
           },
         },
       );
-      const data: PennylaneOAuthResponse = res.data;
+      var data: PennylaneOAuthResponse = res.data;
       await this.prisma.connections.update({
         where: {
           id_connection: connectionId,
